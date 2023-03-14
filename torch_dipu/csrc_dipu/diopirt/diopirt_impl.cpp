e1a619e63330bbc3da56fb4c659d4d36dad4f41c
caikun <caikun@pjlab.org.cn>
2023-03-14 17:15:18 +0800

    Revert "update diopiRequireTensor"
    This reverts commit b08af7e4fb605cad672de5c57eb8ca7f50251c1c.

--- torch_dipu/csrc_dipu/diopirt/diopirt_impl.cpp
+++ torch_dipu/csrc_dipu/diopirt/diopirt_impl.cpp
@@ -81,7 +81,6 @@ DIOPI_RT_API diopiError_t diopiRequireTensor(
     diopiContextHandle_t ctx, diopiTensorHandle_t* tensor,
     const diopiSize_t* size, const diopiSize_t* stride,
     const diopiDtype_t dtype, const diopiDevice_t device) {
-    TORCH_CHECK(tensor != nullptr && *tensor == nullptr, "invalid parameter tensor");
     at::IntArrayRef at_dims(size->data, size->len);
     caffe2::TypeMeta at_type = diopihelper::toATenType(dtype);
     c10::DeviceType at_device = diopihelper::toATenDevice(device);
@@ -108,8 +107,12 @@ DIOPI_RT_API diopiError_t diopiRequireTensor(
     at::IntArrayRef at_strides(stride->data, stride->len);
     at::Tensor t = diopihelper::fromPreAllocated(
         data, at_dims, at_strides, [](void*){}, allocator, options);
-    ctx->arrays.emplace_back(std::move(t));
-    *tensor = reinterpret_cast<diopiTensorHandle_t>(&(ctx->arrays.back()));
+    if ((*tensor) == nullptr) {
+        ctx->arrays.emplace_back(std::move(t));
+        *tensor = reinterpret_cast<diopiTensorHandle_t>(&(ctx->arrays.back()));
+    } else {
+        *(reinterpret_cast<at::Tensor*>(*tensor)) = std::move(t);
+    }
     return diopiSuccess;
 }
 
