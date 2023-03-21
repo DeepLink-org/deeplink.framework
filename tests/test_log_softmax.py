5054f71b254aa2f7c77b8fb4ce01b6c29c085a50
caikun <caikun@pjlab.org.cn>
2023-03-21 11:18:57 +0800

    fix test

--- tests/test_log_softmax.py
+++ tests/test_log_softmax.py
@@ -1,9 +1,10 @@
 import torch
-# import torch_dipu
+import torch_dipu
 
-def test_log_softmax(devicestr : str):
+def test_log_softmax(x, devicestr : str):
     device = torch.device(devicestr)
-    x = torch.randn(2, 3, requires_grad=True)
+    x = x.to(device)
+    x.requires_grad_(True)
     log_softmax_x = torch.nn.functional.log_softmax(x, dim=1)
     print(log_softmax_x)
 
@@ -12,5 +13,6 @@ def test_log_softmax(devicestr : str):
     print(x.grad)
 
 
-# test_log_softmax("dipu")
-test_log_softmax("cpu")
+x = torch.randn(2, 3)
+test_log_softmax(x, "dipu")
+test_log_softmax(x, "cpu")
