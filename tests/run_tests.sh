95e1a1ac41d29d004e6e44d86dcc3d78cd43dc27
zhaochaoxing <zhaochaoxing163@163.com>
2023-04-07 06:06:57 +0000

    config pytorch tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,6 @@
 # !/bin/bash
 set -e
 CDIR="$(cd "$(dirname "$0")" ; pwd -P)"
-# pytorch source directory
-PTDIR=$CDIR/../../pytorch
 
 export TORCH_TEST_DEVICES="$CDIR/pytorch_test_base.py"
 
@@ -17,23 +15,3 @@ function run_coverage {
 function run_test {
   run_coverage "$@"
 }
-
-function run_op_tests {
-  #run_test "$PTDIR/test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA
-  run_test "$PTDIR/test/test_indexing.py" "$@" -v NumpyTestsDIPU
-  run_test "$CDIR/test_ops/test_adaptive_avg_pool2d_backward.py"
-  run_test "$CDIR/test_ops/test_addmm.py"
-  run_test "$CDIR/test_ops/test_log_softmax_backward.py"
-  run_test "$CDIR/test_ops/test_log_softmax.py"
-}
-
-function run_tests {
-  run_op_tests
-#   run_dipu_tests
-}
-
-if [ "$LOGFILE" != "" ]; then
-  run_tests 2>&1 | tee $LOGFILE
-else
-  run_tests
-fi
