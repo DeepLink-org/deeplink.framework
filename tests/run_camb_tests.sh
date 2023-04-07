f8529c2a940f112f37d6c290e32755d738815f43
zhaochaoxing <zhaochaoxing163@163.com>
2023-04-07 06:06:57 +0000

    config pytorch tests

---
+++ tests/run_camb_tests.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+set -e
+
+source run_tests.sh 
+
+function run_dipu_tests {
+  #run_test "${PYTORCH_DIR}/test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA
+  run_test "${PYTORCH_DIR}/test/test_indexing.py" "$@" -v NumpyTestsDIPU
+  run_test "$CDIR/test_ops/test_adaptive_avg_pool2d_backward.py"
+  run_test "$CDIR/test_ops/test_addmm.py"
+  run_test "$CDIR/test_ops/test_log_softmax_backward.py"
+  run_test "$CDIR/test_ops/test_log_softmax.py"
+}
+
+if [ "$LOGFILE" != "" ]; then
+  run_dipu_tests 2>&1 | tee $LOGFILE
+else
+  run_dipu_tests
+fi
