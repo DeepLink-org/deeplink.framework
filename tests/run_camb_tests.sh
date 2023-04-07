aa24dab1b0c240a7325139e34838d65eb295f492
zhaochaoxing <zhaochaoxing163@163.com>
2023-04-07 11:46:06 +0000

    add tests

--- tests/run_camb_tests.sh
+++ tests/run_camb_tests.sh
@@ -4,7 +4,7 @@ set -e
 source tests/run_tests.sh 
 
 function run_dipu_tests {
-  #run_test "${PYTORCH_DIR}/test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA
+  run_test "${PYTORCH_DIR}/test/test_torch.py" "$@" -v TestTorchDeviceTypeDIPU
   run_test "${PYTORCH_DIR}/test/test_indexing.py" "$@" -v NumpyTestsDIPU
   run_test "$CDIR/test_ops/test_adaptive_avg_pool2d_backward.py"
   run_test "$CDIR/test_ops/test_addmm.py"
