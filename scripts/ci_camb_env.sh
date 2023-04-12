e292c200ee47f3ff9aba3fe8a93c439c3d36d5d9
zhaochaoxing <zhaochaoxing163@163.com>
2023-04-12 15:52:54 +0800

    fix tests

--- scripts/ci_camb_env.sh
+++ scripts/ci_camb_env.sh
@@ -1,10 +1,18 @@
-export GCC_ROOT=/mnt/lustre/share/platform/dep/gcc-7.5
+PLATFORM=/mnt/lustre/share/platform
+ENV_NAME=dipu_poc
+export PATH=`python ${PLATFORM}/env/clear_path.py PATH`
+export LD_LIBRARY_PATH=`python ${PLATFORM}/env/clear_path.py LD_LIBRARY_PATH`
+GCC_ROOT=/mnt/lustre/share/platform/dep/gcc-7.5
+CONDA_ROOT=${PLATFORM}/env/miniconda3.8
+export CC=${GCC_ROOT}/bin/gcc
+export CXX=${GCC_ROOT}/bin/g++
+
 export DIOPI_ROOT=$(pwd)/../DIOPI-TEST/lib/no_runtime
 export DIPU_ROOT=$(pwd)/torch_dipu
-export PYTORCH_DIR=/mnt/cache/share/parrotsci/github/cibuild/pytorch
 export LIBRARY_PATH=$DIPU_ROOT:${DIOPI_ROOT}:${LIBRARY_PATH}; LD_LIBRARY_PATH=$DIPU_ROOT:$DIOPI_ROOT:$LD_LIBRARY_PATH
 export PYTHONPATH=${PYTORCH_DIR}/install_path/lib/python3.8/site-packages:${PYTHONPATH}
-export PATH=${PYTORCH_DIR}/install_path/bin:${PATH}
-export LD_PRELOAD=${MPI_ROOT}/lib/libmpi.so:${GCC_ROOT}/lib64/libstdc++.so.6
-export PYTHON_INCLUDE_DIR="/mnt/lustre/share/platform/env/miniconda3.8/envs/pt2.0v1_cpu/include/python3.8"
-alias python='/mnt/lustre/share/platform/env/miniconda3.8/envs/dipu_poc/bin/python'
+export PATH=${PYTORCH_DIR}/install_path/bin:${CONDA_ROOT}/envs/dipu_poc/bin:${CONDA_ROOT}/bin:${PATH}
+export LD_PRELOAD=${GCC_ROOT}/lib64/libstdc++.so.6
+export PYTHON_INCLUDE_DIR="/mnt/lustre/share/platform/env/miniconda3.8/envs/dipu_poc/include/python3.8"
+
+source activate $ENV_NAME
--- tests/run_camb_tests.sh
+++ tests/run_camb_tests.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 set -e
 
-source tests/run_tests.sh
+source tests/common.sh
 export DIPU_TEST_DEVICE="CAMB"
 
 function run_dipu_tests {
@@ -10,6 +10,11 @@ function run_dipu_tests {
   run_test "${PYTORCH_DIR}/test/test_indexing.py" "$@" -v NumpyTestsDIPU
   run_test "${PYTORCH_DIR}/test/test_view_ops.py" "$@" -v TestViewOpsDIPU
   # run_test "${PYTORCH_DIR}/test/test_type_promotion.py" "$@" -v TestTypePromotionDIPU
+  # run_test "${PYTORCH_DIR}/test/test_nn.py" "$@"
+  # run_test "${PYTORCH_DIR}/test/test_ops_fwd_gradients.py" "$@"
+  # run_test "${PYTORCH_DIR}/test/test_ops_gradients.py" "$@"
+  # run_test "${PYTORCH_DIR}/test/test_ops.py" "$@"
+  # run_test "${PYTORCH_DIR}/test/test_shape_ops.py" "$@"
   run_test "$CDIR/test_ops/test_adaptive_avg_pool2d_backward.py"
   run_test "$CDIR/test_ops/test_addmm.py"
   run_test "$CDIR/test_ops/test_log_softmax_backward.py"
