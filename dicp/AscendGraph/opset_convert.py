52557f5430f3cbe490d16449cc76b07e2ccd2354
chenchiyu <chenchiyu@pjlab.org.cn>
2023-04-21 13:55:04 +0800

    change third_party.DICP into dicp due to packaging

--- dicp/AscendGraph/opset_convert.py
+++ dicp/AscendGraph/opset_convert.py
@@ -6,11 +6,10 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from common.op_transformer import OpSetTransformer
-from third_party.DICP.AscendGraph.conversion import patterns, conversions
+from dicp.AscendGraph.conversion import patterns, conversions
 
 
 def ascendgraph_opset_convert(
     gm: torch.fx.GraphModule,
 ):
     return OpSetTransformer(patterns, conversions).transform(gm)
-
