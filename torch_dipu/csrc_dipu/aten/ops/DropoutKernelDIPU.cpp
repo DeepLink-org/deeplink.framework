44dca1439f88a347b8c2ad97c34beca9c47ee751
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-05-09 12:33:28 +0800

    optimize dropout

--- torch_dipu/csrc_dipu/aten/ops/DropoutKernelDIPU.cpp
+++ torch_dipu/csrc_dipu/aten/ops/DropoutKernelDIPU.cpp
@@ -17,12 +17,14 @@ class DipuDropoutFunction
     : public torch::autograd::Function<DipuDropoutFunction> {
 public:
     static at::Tensor forward(torch::autograd::AutogradContext* ctx,
-        const at::Tensor& input, double p, bool train) {
-
-        auto out = at::empty_like(input);
-        auto mask = at::empty(input.sizes(), input.options().dtype(at::kBool));
+        const at::Tensor& input, at::Tensor& out, double p, bool train, bool inplace) {
         at::AutoDispatchBelowADInplaceOrView g;
-        dipu_dropout_impl(input, p, train, out, mask);
+        auto mask = at::empty(input.sizes(), input.options().dtype(at::kBool));
+        if (inplace) {
+            dipu_dropout__impl(out, mask, p, train);
+        } else {
+            dipu_dropout_impl(input, p, train, out, mask);
+        }
         if (train) {
             ctx->saved_data["p"] = p;
             ctx->saved_data["out"] = out;
@@ -50,7 +52,14 @@ public:
 };
 
 at::Tensor dipu_dropout(const at::Tensor& input, double p, bool train) {
-    return DipuDropoutFunction::apply(input, p, train);
+    at::Tensor out = at::empty_like(input);
+    DipuDropoutFunction::apply(input, out, p, train, false);
+    return out;
+}
+
+at::Tensor& dipu__dropout(at::Tensor& input, double p, bool train) {
+    DipuDropoutFunction::apply(input, input, p, train, true);
+    return input;
 }
 
 }  // namespace dipu::native
@@ -59,6 +68,7 @@ namespace at {
 
 TORCH_LIBRARY_IMPL(aten, DIPU_AUTOGRAD_DEVICE_TYPE_MACRO, m) {
     DIOPI_ATEN_FUNC("dropout", ::diopiDropout, dipu::native::dipu_dropout);
+    DIOPI_ATEN_FUNC("dropout_", ::diopiDropoutInp, dipu::native::dipu__dropout);
 }
 
 }  // namespace at
