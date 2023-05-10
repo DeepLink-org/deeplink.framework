e08700a021c61b9b2ad69ec00c8bba4de8f5ca74
Wang.Chenyu <chenyuwang814@ucla.edu>
2023-05-10 09:14:36 +0800

    add access log

--- torch_dipu/csrc_dipu/aten/RegisterDIPU.cpp
+++ torch_dipu/csrc_dipu/aten/RegisterDIPU.cpp
@@ -16,31 +16,37 @@ namespace {
         c10::optional<at::Layout> layout_opt,
         c10::optional<at::Device> device_opt, c10::optional<bool> pin_memory_opt,
         c10::optional<at::MemoryFormat> memory_format_opt) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "empty");
     const DeviceGuard device_guard(device_or_default(device_opt));
     return dnative::empty(size, dtype_opt, layout_opt, device_opt, pin_memory_opt, memory_format_opt);
   }
 
   at::Tensor wrapper_empty_strided(at::IntArrayRef size, at::IntArrayRef stride, c10::optional<at::ScalarType> dtype_opt,
       c10::optional<at::Layout> layout_opt, c10::optional<at::Device> device_opt, c10::optional<bool> pin_memory_opt) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "empty_strided");
     const DeviceGuard device_guard(device_or_default(device_opt));
     return dnative::empty_strided(size, stride, dtype_opt, layout_opt, device_opt, pin_memory_opt);
   }
 
   at::Tensor& wrapper_copy_(at::Tensor& self, const at::Tensor& src, bool non_blocking) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "copy_");
     return dnative::copy_(self, src, non_blocking);
   }
 
   at::Tensor wrapper_DIPU___reshape_alias(const at::Tensor & self, c10::SymIntArrayRef size, c10::SymIntArrayRef stride) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "_reshape_alias");
     return at::native::_reshape_alias(self, C10_AS_INTARRAYREF_SLOW(size), C10_AS_INTARRAYREF_SLOW(stride));
   }
 
   // only used by cpu_fallback.
   at::Tensor wrapper_DIPU___copy_from_and_resize(const at::Tensor & self, const at::Tensor& dst) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "_copy_from_and_resize");
     dst.resize_as_(self).copy_(self);
     return dst;
   }
 
   const at::Tensor& wrapper_resize_(const at::Tensor& self, at::IntArrayRef size, c10::optional<at::MemoryFormat> memory_format) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "resize_");
     // add guard for device switch.
     return dnative::resize_(self, size, memory_format);
   }
@@ -48,94 +54,115 @@ namespace {
   at::Tensor wrapper_DIPU__as_strided(const at::Tensor & self, c10::SymIntArrayRef size, c10::SymIntArrayRef stride, c10::optional<c10::SymInt> storage_offset) {
       // No device check
     // DeviceGuard omitted
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "as_strided");
     return at::native::as_strided_tensorimpl(self, C10_AS_INTARRAYREF_SLOW(size), C10_AS_INTARRAYREF_SLOW(stride), storage_offset.has_value() ? c10::make_optional(storage_offset->expect_int()) : c10::nullopt);
   }
 
   at::Tensor wrapper_DIPU__view(const at::Tensor & self, c10::SymIntArrayRef size) {
     // No device check
     // DeviceGuard omitted
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "view");
     return at::native::view(self, C10_AS_INTARRAYREF_SLOW(size));
   }
 
   at::Tensor wrapper_DIPU__view_as_real(const at::Tensor & self) {
     // DeviceGuard omitted
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "view_as_real");
     return at::native::view_as_real(self);
   }
 
   at::Tensor wrapper_DIPU__view_as_complex(const at::Tensor & self) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "view_as_complex");
     // DeviceGuard omitted
     return at::native::view_as_complex(self);
   }
 
   at::Tensor & wrapper_DIPU__zero_(at::Tensor & self) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "zero_");
     const OptionalDeviceGuard device_guard(device_of(self));
     return at::native::zero_(self);
   }
 
   at::Scalar wrapper_DIPU___local_scalar_dense(const at::Tensor & self) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "_local_scalar_dense");
     const OptionalDeviceGuard device_guard(device_of(self));
     return dnative::_local_scalar_dense_dipu(self);
   }
 
   at::Tensor & wrapper_threshold_backward_out_grad_input(const at::Tensor & grad_output, const at::Tensor & self, const at::Scalar & threshold, at::Tensor & grad_input) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "threshold_backward.grad_input");
     return dnative::threshold_backward_out_grad_input(grad_output, self, threshold, grad_input);
   }
 
   at::Tensor wrapperConvolution2d(
       const at::Tensor & input, const at::Tensor & weight, const c10::optional<at::Tensor> & bias,
       at::IntArrayRef stride, at::IntArrayRef padding, at::IntArrayRef dilation, int64_t groups) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "conv2d");
     return dnative::conv2d(input, weight, bias, stride, padding, dilation, groups);
   }
 
   at::Tensor & wrapperFromRandomInp(at::Tensor & self, int64_t from, c10::optional<int64_t> to, c10::optional<at::Generator> generator) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "random_.from");
     return dnative::random_(self, from, to, generator);
   }
 
   at::Tensor & wrapperToRandomInp(at::Tensor & self, int64_t to, c10::optional<at::Generator> generator) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "random_.to");
     return dnative::random_(self, to, generator);
   }
 
   at::Tensor & wrapperRandomInp(at::Tensor & self, c10::optional<at::Generator> generator) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "random_");
     return dnative::random_(self, generator);
   }
 
 at::Tensor & wrapper_mean_out_out(const at::Tensor & self, at::OptionalIntArrayRef dim, bool keepdim, c10::optional<at::ScalarType> dtype, at::Tensor & out) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "mean.out");
   return dnative::mean_out(self, dim, keepdim, dtype, out);
 }
 
 at::Tensor wrapper_linear(const at::Tensor & input, const at::Tensor & weight, const c10::optional<at::Tensor> & bias) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "linear");
   return dnative::linear(input, weight, bias);
 }
 
 at::Tensor & wrapper__log_softmax_out_out(const at::Tensor & self, int64_t dim, bool half_to_float, at::Tensor & out) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "_log_softmax.out");
   return dnative::_log_softmax_out(self, dim, half_to_float, out);
 }
 
 at::Tensor & wrapper_int_out_log_softmax_out(const at::Tensor & self, int64_t dim, c10::optional<at::ScalarType> dtype, at::Tensor & out) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "log_softmax.int_out");
   return dnative::log_softmax_out(self, dim, dtype, out);
 }
 
 at::Tensor & wrapper__log_softmax_backward_data_out_out(const at::Tensor & grad_output, const at::Tensor & output, int64_t dim, at::ScalarType input_dtype, at::Tensor & out) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "_log_softmax_backward_data.out");
   return dnative::_log_softmax_backward_data_out(grad_output, output, dim, input_dtype, out);
 }
 
 at::Tensor & wrapper_nll_loss_out(const at::Tensor & self, const at::Tensor & target, const c10::optional<at::Tensor> & weight, int64_t reduction, c10::SymInt ignore_index, at::Tensor & out) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "nll_loss.out");
   return dnative::nll_loss_out(self, target, weight, reduction, ignore_index, out);
 }
 
 ::std::tuple<at::Tensor &,at::Tensor &> wrapper_nll_loss_forward_out_output(const at::Tensor & self, const at::Tensor & target, const c10::optional<at::Tensor> & weight, int64_t reduction, int64_t ignore_index, at::Tensor & output, at::Tensor & total_weight) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "nll_loss_forward.output");
   return dnative::nll_loss_forward_out(self, target, weight, reduction, ignore_index, output, total_weight);
 }
 
 at::Tensor & wrapper_nll_loss_backward_out_grad_input(const at::Tensor & grad_output, const at::Tensor & self, const at::Tensor & target, const c10::optional<at::Tensor> & weight, int64_t reduction, int64_t ignore_index, const at::Tensor & total_weight, at::Tensor & grad_input) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "nll_loss_backward.grad_input");
   return dnative::nll_loss_backward_out_grad_input(grad_output, self, target, weight, reduction, ignore_index, total_weight, grad_input);
 }
 
 ::std::tuple<at::Tensor &,at::Tensor &> wrapper_max_pool2d_with_indices_out_out(const at::Tensor & self, at::IntArrayRef kernel_size, at::IntArrayRef stride, at::IntArrayRef padding, at::IntArrayRef dilation, bool ceil_mode, at::Tensor & out, at::Tensor & indices) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "max_pool2d_with_indices.out");
   return dnative::max_pool2d_with_indices_out(self, kernel_size, stride, padding, dilation, ceil_mode, out, indices);
 }
 
 at::Tensor & wrapper_max_pool2d_with_indices_backward_out_grad_input(const at::Tensor & grad_output, const at::Tensor & self, at::IntArrayRef kernel_size, at::IntArrayRef stride, at::IntArrayRef padding, at::IntArrayRef dilation, bool ceil_mode, const at::Tensor & indices, at::Tensor & grad_input) {
+    printf("[%s:%s:%d]:%s\n", __FILE__, __FUNCTION__, __LINE__, "max_pool2d_with_indices_backward.grad_input");
   return dnative::max_pool2d_with_indices_backward_out_grad_input(grad_output, self, kernel_size, stride, padding, dilation, ceil_mode, indices, grad_input);
 }
 
