864db2aa176b9715257b90b663b21fb6407a98c0
jinminxi104 <jinminxi104@hotmail.com>
2023-05-15 19:38:36 +0800

    modify decomp table in tops config.py

--- dicp/TopsGraph/config.py
+++ dicp/TopsGraph/config.py
@@ -4,7 +4,6 @@ from torch._inductor.decomposition import decompositions
 
 aten = torch.ops.aten
 decomp_del_keys = [aten._native_batch_norm_legit_functional.default,
-                   aten.native_batch_norm_backward.default,
                    aten.convolution_backward.default]
 def get_decomp():
     for del_key in decomp_del_keys:
