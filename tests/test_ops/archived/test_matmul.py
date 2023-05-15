673584206b22cb74b1959a306a06c35ac488c5c3
z37903589 <379035389@qq.com>
2023-05-15 15:56:00 +0800

    fix matmul

--- tests/test_ops/archived/test_matmul.py
+++ tests/test_ops/archived/test_matmul.py
@@ -3,7 +3,7 @@ import torch_dipu
 device = torch.device("dipu")
 # 创建两个矩阵
 A = torch.tensor([[1.1, 2], [3, 4]]).to(device)
-B = torch.tensor([[5, 6], [7., 8]]).to(device)
+B = torch.tensor([[1.,], [1,]]).to(device)
 
 # 使用 matmul 函数进行矩阵乘法
 C = torch.matmul(A, B).to(device)
