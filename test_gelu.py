9e53fa6658e261d2659a1afe95e334957f6f8e86
wey-code <1060314685@qq.com>
2023-05-18 16:38:41 +0800

    add floor_div gelu where

---
+++ test_gelu.py
@@ -0,0 +1,14 @@
+import torch
+import torch_dipu
+import torch.nn as nn
+
+device = torch.device("dipu")
+
+m = nn.GELU()
+input = torch.randn(5)
+print(input)
+output = m(input)
+print(output)
+input = input.to(device)
+output = m(input)
+print(output)
