b08187dd65d4e556644e4a28d30e9858383bf4e7
z379035389 <379035389@qq.com>
2023-05-18 15:37:36 +0800

    fix mseloss

--- tests/test_ops/archived/test_mseloss.py
+++ tests/test_ops/archived/test_mseloss.py
@@ -7,6 +7,11 @@ predictions = torch.tensor([0.5, 0.8, 0.2]).to(device)
 targets = torch.tensor([1.0, 0.7, 0.3]).to(device)
 
 # 计算均方误差损失
-loss = torch.nn.functional.mse_loss(predictions, targets)
+loss = torch.nn.functional.mse_loss(predictions, targets,reduction='mean')
+print(loss)
+loss = torch.nn.functional.mse_loss(predictions, targets,reduction='sum')
 # 打印损失值
+print(loss)
+loss = torch.nn.functional.mse_loss(predictions, targets,reduction='none')
+
 print(loss)
