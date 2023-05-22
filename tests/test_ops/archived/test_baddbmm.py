87ed9b91158bf609d8a803abbbdc20f1749d0da4
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-05-22 11:36:06 +0800

    optimize test case of baddbmm

--- tests/test_ops/archived/test_baddbmm.py
+++ tests/test_ops/archived/test_baddbmm.py
@@ -2,11 +2,9 @@
 import torch
 import torch_dipu
 
-M = torch.randn(10, 3, 5).cuda()
-batch1 = torch.randn(10, 3, 4).cuda()
-batch2 = torch.randn(10, 4, 5).cuda()
 
-def test_baddbmm(input, batch1, batch2, *, beta=1, alpha=1, out=None):
+
+def _test_baddbmm(input, batch1, batch2, *, beta=1, alpha=1, out=None):
     cpu_result = torch.baddbmm(input.cpu(), batch1.cpu(), batch2.cpu(), beta = beta, alpha = alpha)
     device_result = torch.baddbmm(input.cuda(), batch1.cuda(), batch2.cuda(), beta = beta, alpha = alpha)
     assert torch.allclose(cpu_result, device_result.cpu())
@@ -19,4 +17,15 @@ def test_baddbmm(input, batch1, batch2, *, beta=1, alpha=1, out=None):
     device_result = input.cuda().baddbmm_(batch1.cuda(), batch2.cuda(), beta = beta, alpha = alpha)
     assert torch.allclose(cpu_result, device_result.cpu())
 
-test_baddbmm(M, batch1, batch2)
+
+def test_baddbmm1():
+    M = torch.randn(10, 3, 5).cuda()
+    batch1 = torch.randn(10, 3, 4).cuda()
+    batch2 = torch.randn(10, 4, 5).cuda()
+    _test_baddbmm(M, batch1, batch2)
+
+def test_baddbmm2():
+    M = torch.randn(10, 3, 5).cuda()
+    batch1 = torch.randn(10, 3, 4).cuda()
+    batch2 = torch.randn(10, 4, 5).cuda()
+    _test_baddbmm(M, batch1, batch2, alpha=2, beta=3)
