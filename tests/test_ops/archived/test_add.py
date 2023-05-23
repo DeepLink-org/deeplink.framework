f69923677fabfc2a5db6b7bda6493dd8020c26b5
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-05-23 10:51:41 +0800

    optimize test case

--- tests/test_ops/archived/test_add.py
+++ tests/test_ops/archived/test_add.py
@@ -4,4 +4,4 @@ import torch_dipu
 x = torch.randn(3,4).cuda()
 y = x.cpu()
 
-assert torch.allclose((x + x).cpu(), y)
+assert torch.allclose((x + x).cpu(), y + y)
--- tests/test_ops/archived/test_fill.py
+++ tests/test_ops/archived/test_fill.py
@@ -6,5 +6,6 @@ x = torch.randn(3,4).cuda()
 y = x.clone()
 
 x.fill_(2)
+y.fill_(2)
 
 assert torch.allclose(x.cpu(), y.cpu())
