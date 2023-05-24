87aeb4cc7fd2884febc69f697541bc4b589820e8
wugeshui <wugeshui@sensetime.com>
2023-05-24 16:54:10 +0800

    add pytorch ci

--- scripts/ci_cuda_env.sh
+++ scripts/ci_cuda_env.sh
@@ -3,5 +3,4 @@ export DIOPI_ROOT=$(pwd)/third_party/DIOPI/DIOPI-IMPL/lib/
 export DIPU_ROOT=$(pwd)/torch_dipu
 export LIBRARY_PATH=$DIPU_ROOT:${DIOPI_ROOT}:${LIBRARY_PATH}; LD_LIBRARY_PATH=$DIPU_ROOT:$DIOPI_ROOT:$LD_LIBRARY_PATH
 export PATH=${CONDA_ROOT}/envs/dipu_poc/bin:${PATH}
-export LD_PRELOAD=${GCC_ROOT}/lib64/libstdc++.so.6
 export PYTHON_INCLUDE_DIR="/opt/conda/envs/py_3.10/include/python3.10/"
