65c0bc4f99d416360e71f7a0db19c565d364c87c
caikun <caikun@pjlab.org.cn>
2023-05-29 13:04:01 +0800

    test one iter tool

--- tests/test_ops/have_bug/test_batch_norm.py
+++ tests/test_ops/have_bug/test_batch_norm.py
@@ -1,6 +1,14 @@
 # Copyright (c) 2023, DeepLink.
+import random
+import numpy as np
+
 import torch
 import torch_dipu
+
+seed = 0
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
 
 device = torch.device("dipu")
 input = torch.randn(2, 3, 4).to(device)
@@ -8,9 +16,8 @@ t = input.view(3, -1)
 m = torch.mean(t, 1)
 v = torch.var(t, 1)
 
-result = torch.nn.functional.batch_norm(input.cpu(), m.cpu(), v.cpu())
+result = torch.nn.functional.batch_norm(input, m, v, training=True)
 print(result)
 
-result = torch.nn.functional.batch_norm(input, m, v)
+result = torch.nn.functional.batch_norm(input.cpu(), m.cpu(), v.cpu(), training=True)
 print(result)
-
