e63059d7e46c8897308e8fb19a48dd1e6b4b5053
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-06-01 11:04:05 +0800

    optimzie test case

--- tests/test_ops/archived/test_matmul.py
+++ tests/test_ops/archived/test_matmul.py
@@ -34,6 +34,7 @@ assert torch.allclose(torch.matmul(tensor1, tensor2), torch.matmul(tensor1.cuda(
 # batched matrix x broadcasted matrix
 tensor1 = torch.randn(20, 10, 3, 4)
 tensor2 = torch.randn(10, 4, 5)
+# camb has problem
 #assert torch.allclose(torch.matmul(tensor1, tensor2), torch.matmul(tensor1.cuda(), tensor2.cuda()).cpu(), atol=1e-3)
 
 tensor1 = torch.randn(4)
