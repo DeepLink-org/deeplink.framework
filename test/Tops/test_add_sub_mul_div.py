29458277675095f450e701b053f21fa0600e2244
yaofengchen <fengchenyao@foxmail.com>
2023-06-02 09:54:54 +0000

    Complete test code of some simple ops.

---
+++ test/Tops/test_add_sub_mul_div.py
@@ -0,0 +1,29 @@
+import torch
+import torch.fx
+
+class MyModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.param = torch.nn.Parameter(torch.rand(3, 4))
+        self.linear = torch.nn.Linear(4, 5)
+
+    def forward(self, a, b):
+        output0 = torch.ops.aten.add(a, b)
+        output1 = torch.ops.aten.sub(output0, a)
+        output2 = torch.ops.aten.mul(output1, b)
+        output3 = torch.ops.aten.div(output2, output0)
+        return output3
+
+a = torch.randn(10, 10)
+b = torch.randn(10, 10)
+
+enflame_model = MyModule()
+compiled_model = torch.compile(enflame_model, backend="topsgraph")
+resenflame = compiled_model(a, b)
+
+torch_model = MyModule()
+restorch = torch_model(a, b)
+
+compare = torch.allclose(resenflame, restorch, equal_nan=True)
+
+print(f'Tests add, sub, mul, div reslut\n{compare}')
