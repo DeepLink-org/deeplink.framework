daec72e010fc95520722cd2cad77de009e468649
wey-code <1060314685@qq.com>
2023-06-06 11:30:23 +0800

    修复创建cuda dataset问题

--- scripts/ci_build_dataset.sh
+++ scripts/ci_build_dataset.sh
@@ -4,12 +4,13 @@
 
 if [ "$1" = "cuda" ]; then
     echo "Executing CUDA operation..."
+    mkdir data
     ln -s /nvme/share/share_data/datasets/classification/imagenet data/imagenet
     ln -s /nvme/share/share_data/datasets/detection/coco  data/coco
     ln -s /nvme/share/share_data/datasets/segmentation/cityscapes data/cityscapes
     ln -s /nvme/share/share_data/datasets/detection3d/kitti data/kitti
     ln -s /nvme/share/share_data/datasets/action/Kinetics400 data/kinetics400
-    ln -s nvme/share/share_data/datasets/ocr/det/icdar2015 data/icdar2015
+    ln -s /nvme/share/share_data/datasets/ocr/det/icdar2015 data/icdar2015
     
 elif [ "$1" = "camb" ]; then
     echo "Executing CAMB operation..."
