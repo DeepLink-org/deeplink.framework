ded946f5aeec1f416966016aea2843973d23b660
william.wang <william.wang@enflame-tech.com>
2023-06-08 08:42:13 +0000

    Add max_pool2d test case

--- tests/test_ops/archived/test_max_pool2d.py
+++ tests/test_ops/archived/test_max_pool2d.py
@@ -2,7 +2,7 @@
 import torch
 import torch_dipu
 
-def test_max_pool2d(x, devicestr : str):
+def test_max_pool2d_with_indices(x, devicestr : str):
     device = torch.device(devicestr)
     x = x.to(device)
     x.requires_grad_(True)
@@ -15,7 +15,22 @@ def test_max_pool2d(x, devicestr : str):
     out.backward(grad_output)
     print(f"x.grad = {x.grad}")
 
+def test_max_pool2d(x, devicestr : str):
+    device = torch.device(devicestr)
+    x = x.to(device)
+    x.requires_grad_(True)
+    kernel_size = (2, 2)
+    stride = (2, 2)
+    out = torch.nn.functional.max_pool2d(x, kernel_size=kernel_size, stride=stride, return_indices=False)
+    print(f"out = {out}")
+
+    grad_output = torch.ones_like(out).to(device)
+    out.backward(grad_output)
+    print(f"x.grad = {x.grad}")
+
 
 x = torch.randn((1, 3, 4, 4))
+test_max_pool2d_with_indices(x, "dipu")
+test_max_pool2d_with_indices(x, "cpu")
 test_max_pool2d(x, "dipu")
 test_max_pool2d(x, "cpu")
