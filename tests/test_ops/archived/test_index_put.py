c1d8cbc97e8eb7ce9ed4dff7282192ae5408aae3
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-06-12 09:30:06 +0800

    optimize test case

--- tests/test_ops/archived/test_index_put.py
+++ tests/test_ops/archived/test_index_put.py
@@ -12,17 +12,15 @@ assert torch.allclose(x.cpu(), torch.zeros_like(x.cpu()))
 
 
 for shape in [(5, 4, 2, 3), (3, 4, 5), (2, 3), (10,)]:
-    input =  torch.randn(shape).clamp(min = -3, max = 3)
-    for numel in range(1, input.numel()):
-        indices_cpu = []
-        indices_device = []
-        for i in range(len(shape)):
-            indice = torch.randint(0, shape[i], (numel,))
-            indices_cpu.append(indice)
-            indices_device.append(indice.cuda())
-            values = torch.randn(numel)
-        y_cpu = torch.index_put(input.clone(), indices_cpu, values, accumulate = True)
-        y_device = torch.index_put(input.cuda(), indices_device, values.cuda(), accumulate = True).cpu()
-
-        assert torch.allclose(y_cpu, y_device.cpu(), atol = 1e-3, rtol = 1e-3), str((y_cpu - y_device).abs().max())
+    values = torch.randn(shape).clamp(min = -3, max = 3) * 100
+    input =  torch.randn(shape).clamp(min = -3, max = 3) * 100
+    indices_cpu = []
+    indices_device = []
+    for i in range(len(shape)):
+        indice = torch.randint(0, min(shape), shape)
+        indices_cpu.append(indice)
+        indices_device.append(indice.cuda())
+    y_cpu = torch.index_put(input.clone(), indices_cpu, values, accumulate = True)
+    y_device = torch.index_put(input.cuda(), indices_device, values.cuda(), accumulate = True).cpu()
+    assert torch.allclose(y_cpu, y_device.cpu(), atol = 1e-3, rtol = 1e-3), str((y_cpu - y_device).abs().max())
 
