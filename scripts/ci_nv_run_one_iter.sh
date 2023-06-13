12d5321fb863134a0db191b13fca5b6656995820
wey-code <1060314685@qq.com>
2023-06-13 14:19:54 +0800

    加入剩余模型，关闭cuda的意外退出，进行测试

--- scripts/ci_nv_run_one_iter.sh
+++ scripts/ci_nv_run_one_iter.sh
@@ -4,20 +4,23 @@ echo  "python path : $PYTHONPATH"
 
 #创建一个二维的列表，分别为train文件位置，配置文件位置，workdir位置和可选参数
 original_list=(
-    "mmpretrain resnet/resnet50_8xb32_in1k.py workdirs_resnet50_8xb32_in1k --no-pin-memory"   
-    # "mmpretrain swin_transformer/swin-large_16xb64_in1k.py workdirs_swin-large_16xb64_in1k --no-pin-memory" 
-    "mmpretrain vision_transformer/vit-base-p16_64xb64_in1k-384px.py workdirs_vit-base-p16_64xb64_in1k-384px --no-pin-memory"  
-    "mmdetection detr/detr_r50_8xb2-150e_coco.py workdirs_detr_r50_8xb2-150e_coco"  
-    "mmdetection yolo/yolov3_d53_8xb8-320-273e_coco.py workdirs_yolov3_d53_8xb8-320-273e_coco" 
-    "mmsegmentation deeplabv3/deeplabv3_r50-d8_4xb2-40k_cityscapes-512x1024.py workdirs_r50-d8_4xb2-40k_cityscapes-512x1024"  
-    # "mmsegmentation unet/unet-s5-d16_fcn_4xb4-160k_cityscapes-512x1024.py workdirs_unet-s5-d16_fcn_4xb4-160k_cityscapes-512x1024" 
-    "mmpose body_2d_keypoint/topdown_heatmap/coco/td-hm_hrnet-w32_udp-8xb64-210e_coco-256x192.py workdirs_td-hm_hrnet-w32_udp-8xb64-210e_coco-256x192" 
-    "mmdetection ssd/ssd300_coco.py workdirs_ssd300_coco" 
-    "mmaction2 recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb.py workdirs_tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb"   
-    "mmpretrain efficientnet/efficientnet-b2_8xb32_in1k.py workdirs_efficientnet-b2_8xb32_in1k --no-pin-memory"  
-    "mmpretrain mobilenet_v3/mobilenet-v3-large_8xb128_in1k.py workdirs_mobilenet-v3-large_8xb128_in1k --no-pin-memory"   
-    "mmsegmentation pspnet/pspnet_r50-d8_4xb2-40k_cityscapes-512x1024.py workdirs_pspnet_r50-d8_4xb2-40k_cityscapes-512x1024" 
-    "mmocr textrecog/crnn/crnn_mini-vgg_5e_mj.py workdirs_crnn_mini-vgg_5e_mj"
+    # "mmpretrain resnet/resnet50_8xb32_in1k.py workdirs_resnet50_8xb32_in1k --no-pin-memory"   
+    # "mmpretrain swin_transformer/swin-large_16xb64_in1k.py workdirs_swin-large_16xb64_in1k --no-pin-memory"   精度问题
+    # "mmpretrain vision_transformer/vit-base-p16_64xb64_in1k-384px.py workdirs_vit-base-p16_64xb64_in1k-384px --no-pin-memory"  
+    # "mmdetection detr/detr_r50_8xb2-150e_coco.py workdirs_detr_r50_8xb2-150e_coco"  
+    # "mmdetection yolo/yolov3_d53_8xb8-320-273e_coco.py workdirs_yolov3_d53_8xb8-320-273e_coco" 
+    # "mmsegmentation deeplabv3/deeplabv3_r50-d8_4xb2-40k_cityscapes-512x1024.py workdirs_r50-d8_4xb2-40k_cityscapes-512x1024"  
+    "mmsegmentation unet/unet-s5-d16_fcn_4xb4-160k_cityscapes-512x1024.py workdirs_unet-s5-d16_fcn_4xb4-160k_cityscapes-512x1024" 
+    # "mmpose body_2d_keypoint/topdown_heatmap/coco/td-hm_hrnet-w32_udp-8xb64-210e_coco-256x192.py workdirs_td-hm_hrnet-w32_udp-8xb64-210e_coco-256x192" 
+    # "mmdetection ssd/ssd300_coco.py workdirs_ssd300_coco" 
+    # "mmaction2 recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb.py workdirs_tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb"   
+    # "mmpretrain efficientnet/efficientnet-b2_8xb32_in1k.py workdirs_efficientnet-b2_8xb32_in1k --no-pin-memory"  
+    # "mmpretrain mobilenet_v3/mobilenet-v3-large_8xb128_in1k.py workdirs_mobilenet-v3-large_8xb128_in1k --no-pin-memory"   
+    # "mmsegmentation pspnet/pspnet_r50-d8_4xb2-40k_cityscapes-512x1024.py workdirs_pspnet_r50-d8_4xb2-40k_cityscapes-512x1024" 
+    # "mmocr textrecog/crnn/crnn_mini-vgg_5e_mj.py workdirs_crnn_mini-vgg_5e_mj"
+    "mmsegmentation deeplabv3plus/deeplabv3plus_r50-d8_4xb2-40k_cityscapes-512x1024.py workdirs_deeplabv3plus_r50-d8_4xb2-40k_cityscapes-512x1024"   
+    "mmagic configs/stable_diffusion/stable-diffusion_ddim_denoisingunet_infer.py workdirs_stable-diffusion_ddim_denoisingunet.py" 
+    "mmpretrain convnext/convnext-small_32xb128_in1k.py workdirs_convnext-small_32xb128_in1k --no-pin-memory"
 )
 
 length=${#original_list[@]}
@@ -62,11 +65,12 @@ for ((i=0; i<$random_model_num; i++)); do
 {
     set -e
     
-    # 记录开始时间（以纳秒为单位）
-    startTime=$(date +%s%N)
 
     pid=$BASHPID  # 存储子进程的PID号
     read -u 796
+
+    # 记录开始时间（以纳秒为单位）
+    startTime=$(date +%s%N)
 
     #锁机制保证有序
     while true; do
@@ -154,35 +158,35 @@ read -r p1 p2 p3 p4 <<< ${selected_list[i]}
 echo "PID: $pid ,name:$p2"  # 输出子进程的PID号
 done
 
-while true; do
-    all_finished=true
-    for index in "${!pids[@]}"; do
-        pid="${pids[index]}"
-        if ! kill -0 "$pid" 2>/dev/null; then
-            # 如果存在 "$pid.done"，那直接删
-            if [ -f "$pid.done" ]; then
-                echo "Child process with PID $pid exited successfully."
-                rm -rf "$pid.done"
-                unset 'pids[index]'  # 删除相应的数组元素
-                continue
-            fi
-            echo "Child process with PID $pid encountered an error. Exiting all child processes."
-            # 结束所有子进程
-            for pid_to_kill in "${pids[@]}"; do
-                kill "$pid_to_kill" 2>/dev/null
-            done
-            exit 1
-        fi
-        all_finished=false
-    done
+# while true; do
+#     all_finished=true
+#     for index in "${!pids[@]}"; do
+#         pid="${pids[index]}"
+#         if ! kill -0 "$pid" 2>/dev/null; then
+#             # 如果存在 "$pid.done"，那直接删
+#             if [ -f "$pid.done" ]; then
+#                 echo "Child process with PID $pid exited successfully."
+#                 rm -rf "$pid.done"
+#                 unset 'pids[index]'  # 删除相应的数组元素
+#                 continue
+#             fi
+#             echo "Child process with PID $pid encountered an error. Exiting all child processes."
+#             # 结束所有子进程
+#             for pid_to_kill in "${pids[@]}"; do
+#                 kill "$pid_to_kill" 2>/dev/null
+#             done
+#             exit 1
+#         fi
+#         all_finished=false
+#     done
 
-    if $all_finished; then
-        break
-    fi
+#     if $all_finished; then
+#         break
+#     fi
 
-    sleep 2  # 适当调整轮询的间隔时间
-done
+#     sleep 2  # 适当调整轮询的间隔时间
+# done
 
-# wait
+wait
 
 echo Done
