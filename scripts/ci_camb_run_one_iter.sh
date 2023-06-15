871de37c33955891a1b16e332b89997d8303c5a7
yangbofun <yangbofun@163.com>
2023-06-15 17:56:16 +0800

    modify diopi and add model

--- scripts/ci_camb_run_one_iter.sh
+++ scripts/ci_camb_run_one_iter.sh
@@ -2,7 +2,15 @@
 
 #创建一个二维的列表，分别为train文件位置，配置文件位置，workdir位置和可选参数
 original_list=(
-    "mmpretrain resnet/resnet50_8xb32_in1k.py workdirs_resnet50_8xb32_in1k --no-pin-memory"  
+    # mmpretrain
+    "mmpretrain resnet/resnet50_8xb32_in1k.py workdirs_resnet50_8xb32_in1k --no-pin-memory"
+    "mmpretrain swin_transformer/swin-large_16xb64_in1k workdirs_swin-large_16xb64_in1k --no-pin-memory"
+    "mmpretrain mobilenet_v2/mobilenet-v2_8xb32_in1k.py workdirs_mobilenet-v2_8xb32_in1k --no-pin-memory"
+    "mmpretrain vision_transformer/vit-base-p16_64xb64_in1k-384px.py workdirs_vit-base-p17_64xb64_in1k-384px --no-pin-memory"
+    # mmdetection
+    "mmdetection yolo/yolov3_d53_8xb8-320-273e_coco.py workdirs_yolov3_d53_8xb8-320-273e_coco --no-pin-memory"
+    "mmdetection faster_rcnn/faster-rcnn_r101_fpn_1x_coco.py workdirs_faster-rcnn_r101_fpn_1x_coco --no-pin-memory"
+    "mmdetection detr/detr_r50_8xb2-150e_coco.py workdirs_detr_r50_8xb2-150e_coco --no-pin-memory"
 )
 
 length=${#original_list[@]}
@@ -29,7 +37,7 @@ done
 mkfifo ./fifo.$$ && exec 796<> ./fifo.$$ && rm -f ./fifo.$$
 for ((i=0; i<$max_parall; i++)); do
     echo  "init add placed row $i" >&796
-done 
+done
 
 pids=()
 
@@ -42,7 +50,7 @@ mkdir one_iter_data
 for ((i=0; i<$random_model_num; i++)); do
 {
     set -e
-    
+
     # 记录开始时间（以纳秒为单位）
     startTime=$(date +%s%N)
 
@@ -65,7 +73,7 @@ for ((i=0; i<$random_model_num; i++)); do
     sh SMART/tools/one_iter_tool/run_one_iter.sh ${train_path} ${config_path} ${work_dir} ${opt_arg}
     sh SMART/tools/one_iter_tool/compare_one_iter.sh
     echo  "after add place row $i"  1>&796
-    touch "$pid.done" 
+    touch "$pid.done"
 
     # 记录结束时间（以纳秒为单位）
     endTime=$(date +%s%N)
