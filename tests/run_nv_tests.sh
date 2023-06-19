57b7ff24b6d8f56a00222ecfdf1ab366dae935ea
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-06-19 15:32:55 +0800

    add nv unary test

--- tests/run_nv_tests.sh
+++ tests/run_nv_tests.sh
@@ -11,7 +11,7 @@ function run_dipu_tests {
   run_test "${PYTORCH_TEST_DIR}/test/test_type_hints.py" "$@" -v
   run_test "${PYTORCH_TEST_DIR}/test/test_type_info.py" "$@" -v
   # run_test "${PYTORCH_TEST_DIR}/test/test_utils.py" "$@" -v
-  # run_test "${PYTORCH_TEST_DIR}/test/test_unary_ufuncs.py" "$@" -v TestUnaryUfuncsDIPU
+  run_test "${PYTORCH_TEST_DIR}/test/test_unary_ufuncs.py" "$@" -v TestUnaryUfuncsDIPU
   # run_test "${PYTORCH_TEST_DIR}/test/test_binary_ufuncs.py" "$@" -v TestBinaryUfuncsDIPU
   # run_test "${PYTORCH_TEST_DIR}/test/test_torch.py" "$@" -v TestTorchDeviceTypeDIPU #--subprocess
   #run_test "${PYTORCH_TEST_DIR}/test/test_indexing.py" "$@" -v TestIndexingDIPU
