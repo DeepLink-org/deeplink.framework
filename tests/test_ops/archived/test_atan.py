a9cee3c4d15405ebd3087c65714d467a2d2e13e7
Gong-air <77915156+Gong-air@users.noreply.github.com>
2023-06-26 13:15:27 +0800

    Update test_atan.py
    change name

--- tests/test_ops/archived/test_atan.py
+++ tests/test_ops/archived/test_atan.py
@@ -6,7 +6,7 @@ dipu = torch.device('dipu')
 cpu = torch.device('cpu')
 
 class TestSchema(unittest.TestCase):
-    def test_cos(self):
+    def test_atan(self):
         input_dipu = torch.Tensor([[1, 2, 4.5, 5,0,-1]]).to(dipu)
         input_cpu = torch.Tensor([[1, 2, 4.5, 5,0,-1]]).to(cpu)
         out_dipu = torch.atan(input_dipu)
