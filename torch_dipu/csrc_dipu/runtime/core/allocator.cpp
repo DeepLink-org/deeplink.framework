6046612d32050f4a949578c0eb169750d1a38ff9
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-06-29 16:58:20 +0800

    delete no need interface

--- torch_dipu/csrc_dipu/runtime/core/allocator.cpp
+++ torch_dipu/csrc_dipu/runtime/core/allocator.cpp
@@ -5,10 +5,6 @@ namespace dipu {
 
 static DIPUAllocator allocator;
 
-c10::Allocator* getDIPUAllocator(void) {
-  return &allocator;
-}
-
 REGISTER_ALLOCATOR(dipu::DIPU_DEVICE_TYPE, &allocator);
 
 }  // namespace dipu
