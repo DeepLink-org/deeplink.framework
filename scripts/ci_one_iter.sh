9dc06077de8b8a5ec15b2685f54613205a68851e
xintian-514 <xintian@pjlab.org.cn>
2023-06-30 14:03:44 +0800

    test diopi on pytorch2.0

--- scripts/ci_one_iter.sh
+++ scripts/ci_one_iter.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
 
-function clone_needed_repo() {
+n clone_needed_repo() {
     # clone some repositories
 
     #define some version
     MMCV_VERSION=9b1209fadbc0d336fa5d0207c2d9f14fb7a4d5fa
     MMENGINE_VERSION=v0.7.4
     MMPRETRAIN_VERSION=xt/test_DIOPI_2.0
-    MMDETECTION_VERSION=dipu_v3.0.0_one_iter_tool
-    MMSEGMENTATION_VERSION=dipu_v1.0.0_one_iter_tool
-    MMPOSE_VERSION=dipu_v1.0.0_one_iter_tool
+    MMDETECTION_VERSION=xt/test_pytorch2.0
+    MMSEGMENTATION_VERSION=xt/test_pytorch2.0
+    MMPOSE_VERSION=xt/test_pytorch2.0
     MMDETECTION3D_VERSION=dipu_v1.1.0_one_iter_tool
     MMACTION2_VERSION=dipu_v1.0.0_one_iter_tool
     MMOCR_VERSION=dipu_v1.0.0_one_iter_tool
@@ -20,24 +19,22 @@ function clone_needed_repo() {
     rm -rf mmpretrain && git clone -b ${MMPRETRAIN_VERSION} https://github.com/DeepLink-org/mmpretrain.git
     rm -rf mmdetection && git clone -b ${MMDETECTION_VERSION} https://github.com/DeepLink-org/mmdetection.git
     rm -rf mmsegmentation && git clone -b ${MMSEGMENTATION_VERSION} https://github.com/DeepLink-org/mmsegmentation.git
-    rm -rf mmpose && git clone -b ${MMPOSE_VERSION} https://github.com/DeepLink-org/mmpose.git
-    rm -rf mmdetection3d && git clone -b ${MMDETECTION3D_VERSION} https://github.com/DeepLink-org/mmdetection3d.git
-    rm -rf mmaction2 && git clone -b ${MMACTION2_VERSION} https://github.com/DeepLink-org/mmaction2.git
-    rm -rf mmocr && git clone -b ${MMOCR_VERSION} https://github.com/DeepLink-org/mmocr.git
-    rm -rf mmagic && git clone -b ${MMAGIC} https://github.com/DeepLink-org/mmagic.git
+    #rm -rf mmpose && git clone -b ${MMPOSE_VERSION} https://github.com/DeepLink-org/mmpose.git
+    #rm -rf mmdetection3d && git clone -b ${MMDETECTION3D_VERSION} https://github.com/DeepLink-org/mmdetection3d.git
+    #rm -rf mmaction2 && git clone -b ${MMACTION2_VERSION} https://github.com/DeepLink-org/mmaction2.git
+    #rm -rf mmocr && git clone -b ${MMOCR_VERSION} https://github.com/DeepLink-org/mmocr.git
+    #rm -rf mmagic && git clone -b ${MMAGIC} https://github.com/DeepLink-org/mmagic.git
     rm -rf mmengine && git clone -b ${MMENGINE_VERSION} https://github.com/open-mmlab/mmengine.git
     rm -rf mmcv && git clone https://github.com/open-mmlab/mmcv.git
     cd mmcv && git checkout ${MMCV_VERSION} && cd ..
 }
 
 function build_needed_repo_cuda() {
-    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    echo $PYTHONPATH
     cd mmcv
     MMCV_WITH_DIOPI=1 MMCV_WITH_OPS=1 python setup.py build_ext -i
     cd ..
-    cd mmagic
-    pip install -e . -v --no-deps
+    #cd mmagic
+    #pip install -e . -v --no-deps
     cd ../mmpretrain
     pip install -e .
     cd ..
@@ -62,7 +59,7 @@ function build_dataset(){
         ln -s /mnt/lustre/share_data/PAT/datasets/mmseg/cityscapes data/cityscapes
         ln -s /mnt/lustre/share_data/PAT/datasets/mmdet3d/kitti data/kitti
         #ln -s /mnt/lustre/share_data/PAT/datasets/Kinetics400 data/kinetics400
-        ln -s /mnt/lustre/share_data/PAT/datasets/mmocr/det/icdar2015/imgs data/icdar2015     
+        ln -s /mnt/lustre/share_data/PAT/datasets/mmocr/icdar2015/imgs data/icdar2015
         ln -s /mnt/lustre/share_data/PAT/datasets/mmocr/recog/Syn90k/mnt/ramdisk/max/90kDICT32px data/mjsynth
         #ln -s /mnt/lustre/share_data/PAT/datasets/stable-diffusion-v1-5 data/stable-diffusion-v1-5
         #ln -s /mnt/lustre/share_data/PAT/datasets/swin_large_patch4_window12_384_22k.pth data/swin_large_patch4_window12_384_22k.pth
