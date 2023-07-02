a43a051137a37088e63c61103a4c1027e7fe2827
xintian-514 <xintian@pjlab.org.cn>
2023-07-02 23:29:01 +0800

    test diopi on pytorch2.0

--- scripts/ci/ci_one_iter.sh
+++ scripts/ci/ci_one_iter.sh
@@ -32,7 +32,7 @@ clone_needed_repo() {
 function build_needed_repo_cuda() {
     cd mmcv
     MMCV_WITH_DIOPI=1 MMCV_WITH_OPS=1 python setup.py build_ext -i
-    cd ..
+    # cd ..
     #cd mmagic
     #pip install -e . -v --no-deps
     cd ../mmpretrain
