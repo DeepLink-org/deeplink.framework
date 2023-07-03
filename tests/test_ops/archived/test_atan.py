8690bca96a06ac84499a77e90339db6236e2e82b
gqw <994321849@qq.com>
2023-07-03 14:23:12 +0800

    code style fix

--- tests/test_ops/archived/test_atan.py
+++ tests/test_ops/archived/test_atan.py
@@ -7,8 +7,8 @@ cpu = torch.device('cpu')
 
 class TestSchema(unittest.TestCase):
     def test_atan(self):
-        input_dipu = torch.Tensor([[1, 2, 4.5, 5,0,-1]]).to(dipu)
-        input_cpu = torch.Tensor([[1, 2, 4.5, 5,0,-1]]).to(cpu)
+        input_dipu = torch.Tensor([[1, 2, 4.5, 5, 0, -1]]).to(dipu)
+        input_cpu = torch.Tensor([[1, 2, 4.5, 5, 0, -1]]).to(cpu)
         out_dipu = torch.atan(input_dipu)
         out_cpu = torch.atan(input_cpu)
         self.assertTrue(torch.allclose(out_dipu.to(cpu), out_cpu))
