6e4b1381f95072eb33387d58ea3b8c0ab077db7e
gqw <994321849@qq.com>
2023-07-04 10:50:37 +0800

    isnan test on cuda

--- tests/test_ops/archived/test_isnan.py
+++ tests/test_ops/archived/test_isnan.py
@@ -1,8 +1,8 @@
 import torch_dipu
 import torch
 
-input_tensor = torch.tensor([1.0, float('nan'), 2.0, 4])
+input_tensor = torch.tensor([1.0, float('nan'), 2.0, 4]).to("cuda")
 output_tensor = torch.isnan(input_tensor)
 expected_output = torch.tensor([False, True, False, False])
 
-assert torch.equal(output_tensor, expected_output),"isnan wrong"
+assert torch.equal(output_tensor.cpu(), expected_output),"isnan wrong"
