1412bca4048cf6523a2f8321576a9fd513da6855
POI-WX <131418410+POI-WX@users.noreply.github.com>
2023-07-05 17:09:13 +0800

    Update test_cdist.py

--- tests/test_ops/archived/test_cdist.py
+++ tests/test_ops/archived/test_cdist.py
@@ -2,37 +2,28 @@ import torch
 import torch_dipu
 from torch_dipu import dipu
 
-
+def test_cdist(plist):
+    a = torch.randn((5,3),requires_grad = True)
+    b = torch.randn((2,3,3),requires_grad = True)
+    
+    a_cuda=a.detach().cuda()
+    b_cuda=b.detach().cuda()
+    a_cuda.requires_grad = True
+    b_cuda.requires_grad = True
+    
+    for p in plist:
+        y = torch.cdist(a, b, p=p)
+        y1 = torch.cdist(a_cuda, b_cuda, p=p)
+        y.backward(torch.ones_like(y))
+        y1.backward(torch.ones_like(y1))
+        assert torch.allclose(y, y1.cpu())
+        assert torch.allclose(a.grad, a_cuda.grad.cpu())
+        assert torch.allclose(b.grad, b_cuda.grad.cpu())
+ 
 if dipu.vendor_type == "MLU":
     # Currently only 1-norm is supported by camb for the scatter op
-    a = torch.randn((5,3),requires_grad = True)
-    b = torch.randn((2,3,3),requires_grad = True)
-    
-    a_cuda=a.detach().cuda()
-    b_cuda=b.detach().cuda()
-    a_cuda.requires_grad = True
-    b_cuda.requires_grad = True
-    
-    y = torch.cdist(a, b, p=1)
-    y1 = torch.cdist(a_cuda, b_cuda, p=1)
-    y.backward(torch.ones_like(y))
-    y1.backward(torch.ones_like(y1))
-    assert torch.allclose(y, y1.cpu())
-    assert torch.allclose(a.grad, a_cuda.grad.cpu())
-    assert torch.allclose(b.grad, b_cuda.grad.cpu())
+    plist = [1]
+    test_cdist(plist)
 else:
-    a = torch.randn((5,3),requires_grad = True)
-    b = torch.randn((2,3,3),requires_grad = True)
-    
-    a_cuda=a.detach().cuda()
-    b_cuda=b.detach().cuda()
-    a_cuda.requires_grad = True
-    b_cuda.requires_grad = True
-    
-    y = torch.cdist(a, b, p=2)
-    y1 = torch.cdist(a_cuda, b_cuda, p=2)
-    y.backward(torch.ones_like(y))
-    y1.backward(torch.ones_like(y1))
-    assert torch.allclose(y, y1.cpu())
-    assert torch.allclose(a.grad, a_cuda.grad.cpu())
-    assert torch.allclose(b.grad, b_cuda.grad.cpu())
+    plist = [1, 2, 0.5, float("inf")]
+    test_cdist(plist)
