a540f57a11bef0e3fbec8b5ffa25ba3e2ec8aa27
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-07-05 13:57:57 +0800

    optimize allocator register

--- torch_dipu/csrc_dipu/runtime/core/DIPUCachingAllocator.cpp
+++ torch_dipu/csrc_dipu/runtime/core/DIPUCachingAllocator.cpp
@@ -6,13 +6,13 @@
 
 namespace dipu {
 
-std::mutex DIPUAllocator::mutex_;
+std::mutex DIPUDeviceAllocator::mutex_;
 
 namespace {
 
-static DIPUAllocator allocator;
+static DIPUDeviceAllocator allocator;
 
-using RegisteredAllocator = std::map<std::string, std::tuple<c10::Allocator*, c10::DeviceType, uint8_t>>;
+using RegisteredAllocator = std::map<c10::DeviceType, std::map<std::string, std::tuple<c10::Allocator*, uint8_t>>>;
 
 static std::unique_ptr<RegisteredAllocator> gDIPURegisterdAllocatorPtr;
 
@@ -28,10 +28,19 @@ void setAllocator(const std::string name, c10::DeviceType device_type, c10::Allo
   if (!gDIPURegisterdAllocatorPtr) {
     gDIPURegisterdAllocatorPtr = std::make_unique<RegisteredAllocator>();
   }
-  (*gDIPURegisterdAllocatorPtr)[name] = std::make_tuple(allocator, device_type, priority);
+  auto& gDIPURegisterdAllocator = *gDIPURegisterdAllocatorPtr;
+  if (gDIPURegisterdAllocator[device_type].count(name) <= 0) {
+    gDIPURegisterdAllocator[device_type][name] = std::make_tuple(allocator, priority);
+  } else {
+    if (std::get<1>(gDIPURegisterdAllocator[device_type][name]) < priority) {
+        gDIPURegisterdAllocator[device_type][name] = std::make_tuple(allocator, priority);
+    } else {
+        TORCH_CHECK(false, "A higher priority allocator is already registered for the same device:", device_type, name, priority);
+    }
+  }
 }
 
-constexpr const char* dipu_default_memcaching_algorithm = "RAW";
+constexpr const char* dipu_default_memcaching_algorithm = "BS";
 
 std::string dipu_memcaching_algorithm = []() {
   const char* env = std::getenv("DIPU_MEMCACHING_ALGORITHM");
@@ -41,17 +50,12 @@ std::string dipu_memcaching_algorithm = []() {
 c10::Allocator*  getAllocator(c10::DeviceType device_type) {
   std::lock_guard<std::mutex> lock(dipu_register_allocator_mutex);
   c10::Allocator* result = nullptr;
-  if (!gDIPURegisterdAllocatorPtr) {
-    if (device_type == dipu::DIPU_DEVICE_TYPE) {
-      return &allocator;
-    }
-  } else {
-    auto iter = gDIPURegisterdAllocatorPtr->find(dipu_memcaching_algorithm);
-    if (iter != gDIPURegisterdAllocatorPtr->end()) {
-      return std::get<0>(iter->second);
-    }
+  auto& gDIPURegisterdAllocator = *gDIPURegisterdAllocatorPtr;
+
+  if (gDIPURegisterdAllocator[device_type].count(dipu_memcaching_algorithm) > 0) {
+    return std::get<0>(gDIPURegisterdAllocator[device_type][dipu_memcaching_algorithm]);
   }
-  TORCH_CHECK(false, "No suitable allocators registered");
+  TORCH_CHECK(false, "No allocator found for the device using the given algorithm:", device_type, dipu_memcaching_algorithm);
   return nullptr;
 }
 
