d3427107afed9e38b7308bfa2694dd5784a44bb2
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-07-05 12:31:03 +0800

    reduce event usage in BC algorithm

--- torch_dipu/csrc_dipu/runtime/core/DIPUBSCachingAllocator.cpp
+++ torch_dipu/csrc_dipu/runtime/core/DIPUBSCachingAllocator.cpp
@@ -17,6 +17,8 @@ class BSCachingAllocator: public CacheAllocator {
 
   mutable std::unordered_map<size_t, std::deque<void*>> idel_blocks_;
   mutable std::unordered_map<void*, DIPUEvent> events_;
+  mutable size_t idel_blocks_num_ = 0;
+  mutable size_t total_blocks_num_ = 0;
   mutable c10::Device device_;
   using mutex_t = std::recursive_mutex;
   mutable mutex_t mutex_;
@@ -46,11 +48,17 @@ public:
     while ((find_count++) < max_find_count) {
       auto  temp_ptr = idel_blocks.front();
       idel_blocks.pop_front();
-      auto& event = events_[temp_ptr];
-      if (event.query()) {
+      const bool event_exist = events_.count(temp_ptr) > 0;
+      if ((!event_exist) || events_[temp_ptr].query()) {
+        if (event_exist) {
+          events_.erase(temp_ptr);
+        }
         ptr = temp_ptr;
-        events_.erase(ptr);
-        DIPU_DEBUG_ALLOCATOR(4, "BSCachingAllocator: reuse " << size << " bytes, ptr:" << ptr << ",block size:" << nbytes << ",allocator:" << this << ",find_count:" << find_count << "/" << max_find_count << ", event num:" << events_.size());
+        DIPU_DEBUG_ALLOCATOR(4, "BSCachingAllocator: reuse " << size << " bytes, ptr:" << ptr << ",block size:" << nbytes << ",allocator:" << this << ",find_count:" << find_count << "/" << max_find_count << ", event num:" << events_.size() << ",idel_blocks_num_:" << idel_blocks_num_ << ",total_blocks_num_" << total_blocks_num_);
+        idel_blocks_num_--;
+        if (idel_blocks_num_ > 16) {
+          recycleEvent();
+        }
         break;
       } else {
         idel_blocks.push_back(temp_ptr);
@@ -62,6 +70,7 @@ public:
       ptr = data_ptr.get();
       device_ = data_ptr.device();
       data_ptr.release_context();
+      total_blocks_num_++;
       DIPU_DEBUG_ALLOCATOR(4, "BSCachingAllocator: allocate " << nbytes << ", requires:" << size << " bytes, ptr:" << ptr << ",allocator:" << this);
     }
 
@@ -75,6 +84,18 @@ public:
     std::lock_guard<mutex_t> lk(mutex_);
     events_[ptr].record();
     idel_blocks_[nbytes].push_back(ptr);
+    idel_blocks_num_++;
+  }
+
+  void recycleEvent() const{
+    for (auto iter = events_.begin(); iter != events_.end();) {
+      auto key = iter->first;
+      auto& event = iter->second;
+      iter++;
+      if (event.query()) {
+        events_.erase(key);
+      }
+    }
   }
 
   void empty_cache() {
@@ -85,6 +106,7 @@ public:
         auto ptr = idel_blocks.front();
         events_[ptr].synchronize();
         raw_allocator()->raw_deallocate(ptr);
+        total_blocks_num_--;
         idel_blocks.pop_front();
         events_.erase(ptr);
       }
