fc5ff83b2a93e7891aeb9b33e464ea4ceea5b6f8
yewentao256 <zhyanwentao@126.com>
2023-07-05 16:37:59 +0800

    feature: add tensor out for clamp

--- tests/test_ops/archived/test_clamp.py
+++ tests/test_ops/archived/test_clamp.py
@@ -11,3 +11,13 @@ assert torch.allclose(x.cpu(), y, atol = 1e-3, rtol = 1e-3)
 x.clamp_max_(0.5)
 y.clamp_max_(0.5)
 assert torch.allclose(x.cpu(), y, atol = 1e-3, rtol = 1e-3)
+
+min = torch.tensor([0.2])
+x.clamp_min_(min.cuda())
+y.clamp_min_(min)
+assert torch.allclose(x.cpu(), y, atol = 1e-3, rtol = 1e-3)
+
+min2 = torch.tensor([0.4])
+torch.clamp_min(x, min2.cuda())
+torch.clamp_min(y, min2)
+assert torch.allclose(x.cpu(), y, atol = 1e-3, rtol = 1e-3)
