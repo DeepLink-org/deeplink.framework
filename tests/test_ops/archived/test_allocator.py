0544b49d2fa0cf928ca2608b96fab275560e8c85
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-07-20 10:48:45 +0800

    enhance allocator test

--- tests/test_ops/archived/test_allocator.py
+++ tests/test_ops/archived/test_allocator.py
@@ -1,7 +1,7 @@
 import os
 from multiprocessing import Process
 
-def test_allocator(max_allocate, algorithm, log_mask):
+def test_allocator(max_allocate, step, algorithm, log_mask, test_pin_memory = True):
     os.environ['DIPU_DEVICE_MEMCACHING_ALGORITHM'] = algorithm
     os.environ['DIPU_DEBUG_ALLOCATOR'] = str(log_mask)
     os.environ['DIPU_MEM_CHECK'] = '1'
@@ -11,7 +11,7 @@ def test_allocator(max_allocate, algorithm, log_mask):
     import time
 
     start = time.time()
-    for nbytes in range(0, max_allocate):
+    for nbytes in range(0, max_allocate, step):
         if nbytes % 1024 == 0:
             print(f"allocate {nbytes} nbytes use {algorithm}")
         raw = torch.empty(size = (nbytes,), dtype = torch.uint8, device = 'dipu')
@@ -24,8 +24,11 @@ def test_allocator(max_allocate, algorithm, log_mask):
     end = time.time()
     print(f"allocate [0, {max_allocate}) bytes device memory use {algorithm} success, elasped: {end - start} seconds")
 
+    if test_pin_memory == False:
+        return
+
     start = time.time()
-    for nbytes in range(0, max_allocate):
+    for nbytes in range(0, max_allocate, step):
         if nbytes % 1024 == 0:
             print(f"allocate {nbytes} nbytes use {algorithm}")
         raw = torch.empty(size = (nbytes,), dtype = torch.uint8)
@@ -43,18 +46,24 @@ def test_allocator(max_allocate, algorithm, log_mask):
 
 if __name__=='__main__':
     max_allocate = 1 << 15
-    p1 = Process(target = test_allocator, args = (max_allocate, "BF", 0),)
+    p1 = Process(target = test_allocator, args = (max_allocate, 1, "BF", 0),)
     p1.start()
     p1.join()
 
-    p2 = Process(target = test_allocator, args = (max_allocate, "BS", 0),)
+    p2 = Process(target = test_allocator, args = (max_allocate, 1, "BS", 0),)
     p2.start()
     p2.join()
 
-    p3 = Process(target = test_allocator, args = (max_allocate, "RAW", 0))
+    p3 = Process(target = test_allocator, args = (max_allocate, 1, "RAW", 0))
     p3.start()
     p3.join()
+
+    max_allocate = 1 << 30
+    p4 = Process(target = test_allocator, args = (max_allocate, 17919, "BF", 3, False),)
+    p4.start()
+    p4.join()
 
     assert p1.exitcode == 0
     assert p2.exitcode == 0
     assert p3.exitcode == 0
+    assert p4.exitcode == 0
