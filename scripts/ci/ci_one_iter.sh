fea7fe073cc614f0d5e1fe31736f8300275beab9
slc <1060314685@qq.com>
2023-07-20 16:05:36 +0800

    pip install mmyolo --no-deps

--- scripts/ci/ci_one_iter.sh
+++ scripts/ci/ci_one_iter.sh
@@ -37,12 +37,12 @@ function build_needed_repo_cuda() {
     cd mmcv
     MMCV_WITH_DIOPI=1 MMCV_WITH_OPS=1 python setup.py build_ext -i
     cd ../mmdet 
-    pip install -e .
+    pip install -e . --no-deps
     cd ../mmyolo
     # Install albumentations
-    pip install -r requirements/albu.txt 
+    pip install -r requirements/albu.txt --no-deps
     # Install MMYOLO
-    pip install -e .
+    pip install -e . --no-deps
     cd ..
     # cd mmagic
     # pip install -e . -v --no-deps
