3d7a0dfac3940f5d120b23d731935acb3e4ba179
zhaochaoxing <zhaochaoxing163@163.com>
2023-07-31 12:37:00 +0000

    uncommit ascend tests

--- test/Ascend/test_bmm.py
+++ test/Ascend/test_bmm.py
@@ -1,16 +1,4 @@
 import torch
-import acl
-
-def check_ret(message, ret):
-    if ret != 0:
-        raise Exception("{} failed ret={}"
-                        .format(message, ret))
-
-# ret = acl.init()
-# check_ret("acl.init", ret)
-
-# ret = acl.rt.set_device(0)
-# check_ret("acl.rt.set_device", ret)
     
 input = torch.randn(3, 3, 4)
 mat2 = torch.randn(3, 4, 5)
@@ -20,15 +8,8 @@ def fn(input, mat2):
     return y
 
 opt_model = torch.compile(fn, backend='ascendgraph')
-#opt_model = torch.compile(fn, backend='inductor')
 
 y = opt_model(input, mat2)
 print(y)
 print(y.shape)
-
-
-# ret = acl.rt.reset_device(0)
-# check_ret("acl.rt.reset_device", ret)
-# ret = acl.finalize()
-# check_ret("acl.finalize", ret)
 print('Resources released successfully.')
--- test/Ascend/test_resnet18.py
+++ test/Ascend/test_resnet18.py
@@ -26,19 +26,6 @@ resnet18_cfg = Config.fromfile(
     os.path.join(config_root, '_base_/models/resnet18.py')
 )
 
-def check_ret(message, ret):
-    if ret != 0:
-        raise Exception("{} failed ret={}"
-                        .format(message, ret))
-
-import acl
-device_id = 0
-ret = acl.init()
-check_ret("acl.init", ret)
-
-ret = acl.rt.set_device(device_id)
-check_ret("acl.rt.set_device", ret)
-
 # data
 inputs = torch.randn(4, 3, 224, 224)
 gt = torch.randint(100, (4,), dtype=torch.int64) # 100 means [0, 100)
@@ -50,9 +37,4 @@ compiled_model = torch.compile(model.forward_train, backend='ascendgraph')
 loss = compiled_model(inputs, gt)
 loss['loss'].backward()
 
-ret = acl.rt.reset_device(device_id)
-check_ret("acl.rt.reset_device", ret)
-
-ret = acl.finalize()
-check_ret("acl.finalize", ret)
 print('Resources released successfully.')
