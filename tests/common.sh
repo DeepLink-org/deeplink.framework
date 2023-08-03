b98d2543eea552ef5a06c894036f7ddbd1c4d225
liwenjian-sensetime <109193776+liwenjian-sensetime@users.noreply.github.com>
2023-08-03 10:48:52 +0800

    Update common.sh

--- tests/common.sh
+++ tests/common.sh
@@ -3,7 +3,6 @@ set -e
 CDIR="$(cd "$(dirname "$0")" ; pwd -P)"
 
 export TORCH_TEST_DEVICES="$CDIR/pytorch_test_base.py"
-export USE_COVERAGE=ON
 function run_coverage {
   if [ "$USE_COVERAGE" == "ON" ]; then
     coverage run --source="$TORCH_DIPU_DIR" -p "$@"
