9c2e8a937b342b693dd94804b0936534829fc265
slc <1060314685@qq.com>
2023-08-05 20:29:19 +0800

    modify ci cuda models

--- scripts/ci/ci_one_iter.sh
+++ scripts/ci/ci_one_iter.sh
@@ -97,7 +97,7 @@ function build_dataset(){
         ln -s /mnt/lustre/share_data/parrots.tester.s.03/dataset/data_for_ln/imagenet data/imagenet
         ln -s /mnt/lustre/share_data/parrots.tester.s.03/dataset/data_for_ln/coco  data/coco
         ln -s /mnt/lustre/share_data/parrots.tester.s.03/dataset/data_for_ln/cityscapes data/cityscapes
-        # ln -s /mnt/lustre/share_data/parrots.tester.s.03/dataset/data_for_ln/kinetics400 data/kinetics400  #数据集还在迁移
+        ln -s /mnt/lustre/share_data/parrots.tester.s.03/dataset/mmaction2/Kinetics400 data/kinetics400 
         ln -s /mnt/lustre/share_data/parrots.tester.s.03/dataset/data_for_ln/icdar2015 data/icdar2015
         ln -s /mnt/lustre/share_data/parrots.tester.s.03/dataset/data_for_ln/mjsynth data/mjsynth
         ln -s /mnt/lustre/share_data/parrots.tester.s.03/dataset/data_for_ln/kitti data/kitti
