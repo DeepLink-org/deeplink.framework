275d534b1528f1699cf4264e2f188cd9c94bbf2c
Lingjie Li <lilingjie@sensetime.com>
2023-09-11 14:50:36 +0800

    write better test_prod

--- tests/test_ops/archived/test_prod.py
+++ tests/test_ops/archived/test_prod.py
@@ -1,31 +1,33 @@
 import torch
 import torch_dipu
+from torch_dipu.testing._internal.common_utils import TestCase, run_tests
 
-x = torch.Tensor([[1, 2, 3], [4, 5, 6]])
-x_xpu = x.cuda()
-print(x_xpu.cpu())
 
-p = torch.prod(x)
-p_xpu = torch.prod(x_xpu)
-print(p_xpu.cpu())
-assert torch.allclose(p_xpu.cpu(), p)
+class TestProd(TestCase):
+    def _test_prod_with_args(self, input_tensor, *args, **kwargs):
+        out = torch.prod(input_tensor, *args, **kwargs)
+        out_cuda = torch.prod(input_tensor.cuda(), *args, **kwargs)
+        self.assertRtolEqual(out, out_cuda.cpu())
 
-p = torch.prod(x, 0)
-p_xpu = torch.prod(x_xpu, 0)
-print(p_xpu.cpu())
-assert torch.allclose(p_xpu.cpu(), p)
+    def test_prod(self):
+        input_tensor = torch.tensor([[1, 2, 3], [4, 5, 6]])
+        self._test_prod_with_args(input_tensor)
 
-p = torch.prod(x, 1)
-p_xpu = torch.prod(x_xpu, 1)
-print(p_xpu.cpu())
-assert torch.allclose(p_xpu.cpu(), p)
+    def test_prod_dim_int(self):
+        input_tensor = torch.tensor([[1, 2, 3], [4, 5, 6]])
+        self._test_prod_with_args(input_tensor, 0)
+        self._test_prod_with_args(input_tensor, 1)
+        self._test_prod_with_args(input_tensor, 0, True)
+        self._test_prod_with_args(input_tensor, 1, True)
 
-p = torch.prod(x, 0, True)
-p_xpu = torch.prod(x_xpu, 0, True)
-print(p_xpu.cpu())
-assert torch.allclose(p_xpu.cpu(), p)
+    def test_prod_bool(self):
+        input_arrays = [[True, True], [True, False], [False, False]]
+        for input_array in input_arrays:
+            input_tensor = torch.tensor(input_array)
+            out = torch.prod(input_tensor).item()
+            out_cuda = torch.prod(input_tensor.cuda()).item()
+            self.assertTrue(out == out_cuda)
 
-p = torch.prod(x, 1, True)
-p_xpu = torch.prod(x_xpu, 1, True)
-print(p_xpu.cpu())
-assert torch.allclose(p_xpu.cpu(), p)
+
+if __name__ == "__main__":
+    run_tests()
