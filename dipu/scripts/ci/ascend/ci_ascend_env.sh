3b9b6e145b0bafb244de980b0ec4bf38df83ef80
jfxu-st <xujinfan@sensetime.com>
2023-09-19 15:26:40 +0800

    update environment variable settings in ci_ascend_env.sh

--- dipu/scripts/ci/ascend/ci_ascend_env.sh
+++ dipu/scripts/ci/ascend/ci_ascend_env.sh
@@ -1,6 +1,7 @@
+ENV_PATH=/mnt/cache/share/platform/cienv
+source ${ENV_PATH}/pt2.0_ci
+
 export DIOPI_ROOT=$(pwd)/third_party/DIOPI/impl/lib
 export DIPU_ROOT=$(pwd)/torch_dipu
-export LIBRARY_PATH=$DIPU_ROOT:$DIOPI_ROOT:$LIBRARY_PATH 
-export LD_LIBRARY_PATH=$DIPU_ROOT:$DIOPI_ROOT:$LD_LIBRARY_PATH
-export PYTORCH_DIR=${CONDA_PREFIX}/lib/python3.8/site-packages
-export PYTHONPATH=$(pwd)/../pytorch:$(pwd):$PYTHONPATH
+export LIBRARY_PATH=${DIPU_ROOT}:${DIOPI_ROOT}:${LIBRARY_PATH}
+export LD_LIBRARY_PATH=${DIPU_ROOT}:${DIOPI_ROOT}:${LD_LIBRARY_PATH}
