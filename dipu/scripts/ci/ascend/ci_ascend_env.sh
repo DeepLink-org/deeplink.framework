cd1e4fe6b2db80988f7fe393e0a53c4525cacae8
jfxu-st <xujinfan@sensetime.com>
2023-09-19 15:28:39 +0800

    add PyTorch compiling on ASCEND_CLUSTER to the Rsync job

--- dipu/scripts/ci/ascend/ci_ascend_env.sh
+++ dipu/scripts/ci/ascend/ci_ascend_env.sh
@@ -5,3 +5,7 @@ export DIOPI_ROOT=$(pwd)/third_party/DIOPI/impl/lib
 export DIPU_ROOT=$(pwd)/torch_dipu
 export LIBRARY_PATH=${DIPU_ROOT}:${DIOPI_ROOT}:${LIBRARY_PATH}
 export LD_LIBRARY_PATH=${DIPU_ROOT}:${DIOPI_ROOT}:${LD_LIBRARY_PATH}
+export PYTHONPATH=$(pwd):${PYTHONPATH}
+
+export PYTORCH_DIR=${ASCEND_TORCH_DIR}
+export PYTHONPATH=${PYTORCH_DIR}:${PYTHONPATH}
