9a4acef507d5ccc6feade0a452d233f0c76787c2
jfxu-st <xujinfan@sensetime.com>
2023-09-20 20:19:16 +0800

    add a Test-One-Iter-Ascend job which has only a 'Build some env' step

--- dipu/scripts/ci/ascend/ci_ascend_env.sh
+++ dipu/scripts/ci/ascend/ci_ascend_env.sh
@@ -7,5 +7,10 @@ export LIBRARY_PATH=${DIPU_ROOT}:${DIOPI_ROOT}:${LIBRARY_PATH}
 export LD_LIBRARY_PATH=${DIPU_ROOT}:${DIOPI_ROOT}:${LD_LIBRARY_PATH}
 export PYTHONPATH=$(pwd):${PYTHONPATH}
 
+# MMCV build requirements
+export DIOPI_PATH=$(pwd)/third_party/DIOPI/proto
+export DIPU_PATH=${DIPU_ROOT}
+
+# PyTorch path related settings
 export PYTORCH_DIR=${ASCEND_TORCH_DIR}
 export PYTHONPATH=${PYTORCH_DIR}:${PYTHONPATH}
