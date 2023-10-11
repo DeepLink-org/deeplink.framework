8251288409b1fc57637a8f6a4e149d86d8481a8e
HuayiL <442488254@qq.com>
2023-10-11 17:43:43 +0800

    This change is used to help users build and install DIPU from source code and pip in one step.

--- dipu/setup.py
+++ dipu/setup.py
@@ -9,7 +9,7 @@ import subprocess
 import sys
 import platform
 import setuptools
-from setuptools import setup, distutils, Extension
+from skbuild import setup
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 VERSION = '0.1'
@@ -18,12 +18,24 @@ def get_pytorch_dir():
     import torch
     return os.path.dirname(os.path.abspath(torch.__file__))
 
+
+def customized_cmake_args():
+    cmake_args = list()
+    cmake_args.append("-DCMAKE_BUILD_TYPE=Release")
+    cmake_args.append("-DDEVICE=cuda")
+    cmake_args.append("-DENABLE_COVERAGE=${USE_COVERAGE}")
+    return cmake_args
+
 setup(
     name="torch_dipu",
     version=VERSION,
     description='DIPU extension for PyTorch',
     url='https://github.com/DeepLink-org/dipu',
-    packages=["torch_dipu"],
+    packages=setuptools.find_packages(),
+    cmake_args=customized_cmake_args(),
+    cmake_install_target="all",
+    package_data={},
+    package_dir={},
     ext_modules=[
     ],
     cmdclass={
