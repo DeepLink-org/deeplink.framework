fb44c31675beede9de01e0aedce52c5ef5b81e2d
HuayiL <442488254@qq.com>
2023-10-16 10:43:11 +0800

    fix a bug with the ENV DIPU_COMPILED_WITH_CXX11

--- dipu/setup.py
+++ dipu/setup.py
@@ -48,7 +48,27 @@ def customized_cmake_args():
     cmake_args.append("-DCMAKE_BUILD_TYPE=Release")
     cmake_args.append("-DDEVICE=cuda")
     cmake_args.append("-DENABLE_COVERAGE=${USE_COVERAGE}")
+    cmake_args.append("-DBUILD_DIOPI=TRUE")
     return cmake_args
+
+def torch_dipu_headers():
+    headers_pattern = list()
+    headers_pattern.append("csrc_dipu/*.h")
+    headers_pattern.append("csrc_dipu/aten/*.h")
+    headers_pattern.append("csrc_dipu/base/*.h")
+    headers_pattern.append("csrc_dipu/binding/*.h")
+    headers_pattern.append("csrc_dipu/diopirt/*.h")
+    headers_pattern.append("csrc_dipu/profiler/*.h")
+    headers_pattern.append("csrc_dipu/utils/*.h")
+    headers_pattern.append("csrc_dipu/runtime/*.h")
+    headers_pattern.append("csrc_dipu/runtime/core/*.h")
+    headers_pattern.append("csrc_dipu/runtime/core/allocator/*.h")
+    headers_pattern.append("csrc_dipu/runtime/core/guardimpl/*.h")
+    headers_pattern.append("csrc_dipu/runtime/device/*.h")
+    headers_pattern.append("csrc_dipu/runtime/devproxy/*.h")
+    headers_pattern.append("csrc_dipu/runtime/distributed/*.h")
+    headers_pattern.append("csrc_dipu/vendor/*/*.h")
+    return headers_pattern
 
 setup(
     name="torch_dipu",
@@ -58,8 +78,7 @@ setup(
     packages=setuptools.find_packages(),
     cmake_args=customized_cmake_args(),
     cmake_install_target="all",
-    package_data={},
-    package_dir={},
+    package_data = {'torch_dipu':["*.lib","*.so","*.pylib","../third_party/DIOPI/impl/lib/*.lib","../third_party/DIOPI/impl/lib/*.so","../third_party/DIOPI/impl/lib/*.pylib",]+torch_dipu_headers(),},
     ext_modules=[
     ],
     cmdclass={
