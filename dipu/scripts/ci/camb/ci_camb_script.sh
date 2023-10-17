9267e291e9a0eff90938f81dfea4eac8de13bbc9
lhy <442499254@qq.com>
2023-10-17 16:25:10 +0800

    update the ci_camb_script.sh

--- dipu/scripts/ci/camb/ci_camb_script.sh
+++ dipu/scripts/ci/camb/ci_camb_script.sh
@@ -2,13 +2,6 @@
 set -e
 echo "pwd: $(pwd)"
 
-function build_dipu_py() {
-    echo "building dipu_py:$(pwd)"
-    export CMAKE_BUILD_TYPE=Release
-    export MAX_JOBS=12
-    python setup.py build_ext 2>&1 | tee ./setup.log
-    mv build/python_ext/torch_dipu/_C.cpython-*.so torch_dipu
-}
 
 function config_dipu_camb_cmake() {
     mkdir -p build && cd ./build && rm -rf ./*
@@ -50,27 +43,20 @@ function build_dipu_lib() {
     export LIBRARY_PATH=$DIOPI_ROOT:$LIBRARY_PATH;
     config_dipu_camb_cmake 2>&1 | tee ./cmake_camb.log
     cd build && make -j8  2>&1 | tee ./build.log &&  cd ..
-    mv ./build/torch_dipu/csrc_dipu/libtorch_dipu.so   ./torch_dipu
-    mv ./build/torch_dipu/csrc_dipu/libtorch_dipu_python.so   ./torch_dipu
 }
 
 case $1 in
     build_dipu)
         (
             build_diopi_lib
-            autogen_diopi_wrapper
             build_dipu_lib
-            build_dipu_py
         ) || exit -1;;
     build_diopi)
         build_diopi_lib || exit -1;;
     build_autogen_diopi_wrapper)
         autogen_diopi_wrapper || exit -1;;
     build_dipu_only)
-        (
-         autogen_diopi_wrapper
-         build_dipu_lib
-         build_dipu_py) || exit -1;;
+        build_dipu_lib || exit -1;;
     *)
         echo -e "[ERROR] Incorrect option:" $1;
 esac
