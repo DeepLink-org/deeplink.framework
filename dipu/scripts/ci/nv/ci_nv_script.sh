9c8054b8c0296018a1daae229ba4fab4900ef355
lhy <442488254@qq.com>
2023-10-31 17:57:45 +0800

    add "import torch" in setup.py for setting related CMake args. Also pipeclean for "pip install ."

--- dipu/scripts/ci/nv/ci_nv_script.sh
+++ dipu/scripts/ci/nv/ci_nv_script.sh
@@ -12,15 +12,15 @@ function config_dipu_nv_cmake() {
     cd ../
 }
 
-function build_diopi_lib() {
-    cd third_party/DIOPI/
-    cd impl
-    which cmake
-    sh scripts/build_impl.sh clean
-    sh scripts/build_impl.sh torch || exit -1
-
-    cd ../../..
-    unset Torch_DIR
+function config_all_nv_cmake() {
+    # export NCCL_ROOT="you nccl path should exist"
+    
+    mkdir -p build && cd ./build && rm -rf ./*
+    cmake ../  -DCMAKE_BUILD_TYPE=Release \
+        -DDEVICE=cuda \
+        -DENABLE_COVERAGE=${USE_COVERAGE} \
+        -DWITH_DIOPI=INTERNAL
+    cd ../
 }
 
 function build_dipu_lib() {
@@ -31,11 +31,17 @@ function build_dipu_lib() {
     cd build && make -j8  2>&1 | tee ./build.log &&  cd ..
 }
 
+function build_all(){
+    echo "building dipu_lib:$(pwd)"
+    echo  "DIOPI_ROOT:${DIOPI_ROOT}"
+    export DIOPI_BUILD_TESTRT=1
+    config_all_nv_cmake 2>&1 | tee ./cmake_nv.log
+    cd build && make -j8  2>&1 | tee ./build.log &&  cd ..
+}
 case $1 in
     build_dipu)
         (
-            build_diopi_lib
-            build_dipu_lib
+            build_all
         ) \
         || exit -1;;
     build_dipu_only)
--- dipu/setup.py
+++ dipu/setup.py
@@ -9,16 +9,30 @@ import subprocess
 import sys
 import platform
 import setuptools
-import torch
 from skbuild import setup
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 VERSION = '0.1'
 
-def get_pytorch_dir():
+try:
     import torch
-    return os.path.dirname(os.path.abspath(torch.__file__))
+except:
+    sys.path.append(os.environ['PYTORCH_DIR'])
+    print("PYTHONPATH: "+str(sys.path))
+import torch
+import builtins
 
+def get_DIPU_ABI_V():
+    return (next(item[-4:-2] for item in dir(builtins) if "__pybind11_internals_v4_gcc_libstdcpp_cxxabi10" in item))
+
+def get_DIPU_COMPILED_WITH_CXX11_ABI():
+    return (1 if torch.compiled_with_cxx11_abi() else 0)
+
+def get_DIOPI_CMAKE_PREFIX_PATH():
+    return torch.utils.cmake_prefix_path
+
+def get_PYTORCH_DIR():
+    return os.path.dirname(torch.__path__[0])
 
 def customized_cmake_args():
     cmake_args = list()
@@ -28,7 +42,10 @@ def customized_cmake_args():
     cmake_args.append("-DCMAKE_BUILD_TYPE=Release")
     cmake_args.append("-DDEVICE="+cmake_device)
     cmake_args.append("-DENABLE_COVERAGE=${USE_COVERAGE}")
-    cmake_args.append("-DBUILD_DIOPI=TRUE")
+    cmake_args.append("-DDIPU_ABI_V="+get_DIPU_ABI_V())
+    cmake_args.append("-DDIPU_COMPILED_WITH_CXX11_ABI="+str(get_DIPU_COMPILED_WITH_CXX11_ABI()))
+    cmake_args.append("-DDIOPI_CMAKE_PREFIX_PATH="+get_DIOPI_CMAKE_PREFIX_PATH())
+    cmake_args.append("-DPYTORCH_DIR="+get_PYTORCH_DIR())
     cmake_args.append("-DWITH_DIOPI=INTERNAL")
     return cmake_args
 
