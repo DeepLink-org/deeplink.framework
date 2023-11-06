1fd2786c1909daa6260f04721c8f53dad7a00428
jinminxi104 <jinminxi104@hotmail.com>
2023-11-06 21:02:04 +0800

    adjust err msg of test script

--- test/model/run_test_model.sh
+++ test/model/run_test_model.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
 # export TOPS_VISIBLE_DEVICES=0
-# TEST_DIR=$(dirname $(dirname $(dirname $(readlink -f $0))))
-# LLAMA_MODEL_DIR=/home/cse/projects/pujiang/llama_infer/models
-if [ ! $TEST_DIR ] || [ ! $LLAMA_MODEL_DIR ]; then
-    if [ ! $TEST_DIR ]; then
-        echo "TEST_DIR is not defined!" >&2
-    fi
-    if [ ! $LLAMA_MODEL_DIR ]; then
-        echo "LLAMA_MODEL_DIR is not defined!" >&2
-    fi
+if [ ! $TEST_DIR ]; then
+    echo "TEST_DIR is not defined!" >&2
     exit 1
 fi
+
 
 TEST_MODEL_DIR=${TEST_DIR}/model
 TEST_MODEL=$1
 BACKEND=$2
 DYNAMIC=$3
+
+if [ "$TEST_MODEL" == "llama" ] && [ ! $LLAMA_MODEL_DIR ]; then
+    echo "LLAMA_MODEL_DIR is not defined!" >&2
+    exit 1
+fi
 
 cd ${TEST_MODEL_DIR}
 if [ ${DYNAMIC} == false ] || [ ${DYNAMIC} == true ]; then
