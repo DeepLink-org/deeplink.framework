e83db3d1abc6c99d6ca2c21b544e61a403a6dc99
fandaoyi <fandaoyi@sensetime.com>
2023-11-30 11:25:55 +0800

    fix recycle dep (#474)

---
+++ dipu/torch_dipu/csrc_dipu/runtime/core/allocator/DIPUCachingAllocatorUtils.h
@@ -0,0 +1,28 @@
+
+#pragma once
+
+#include <c10/core/Allocator.h>
+#include <c10/core/Device.h>
+
+#include "csrc_dipu/runtime/core/DIPUStream.h"
+
+namespace dipu {
+size_t memoryReserved(const c10::Device& device);
+
+size_t memoryAllocated(const c10::Device& device);
+
+size_t maxMemoryReserved(const c10::Device& device);
+
+size_t maxMemoryAllocated(const c10::Device& device);
+
+void emptyCachedMem();
+
+void initCachedAllocator();
+
+void releaseAllDeviceMem();
+
+void recordStream(const c10::DataPtr& ptr, DIPUStream stream);
+
+void recordStream(const at::Tensor& tensor, DIPUStream stream);
+
+}  // namespace dipu
