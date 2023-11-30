f32eb14363b9e68dbaeb1323e35a9b859a80a6cc
jingguo-st <fujingguo@sensetime.com>
2023-11-30 13:03:42 +0800

    add floor_ and cases for floor_, ceil and ceil_

--- dipu/tests/python/unittests/test_ceil.py
+++ dipu/tests/python/unittests/test_ceil.py
@@ -4,22 +4,22 @@ import torch_dipu
 from torch_dipu.testing._internal.common_utils import TestCase, run_tests
 
 
-class TestSchema(TestCase):
-    def test_floor(self):
+class TestCeil(TestCase):
+    def test_ceil(self):
         dipu = torch.device("dipu")
         cpu = torch.device("cpu")
         x = torch.randn(4)
-        z1 = torch.floor(x)
-        z2 = torch.floor(x.to(dipu))
+        z1 = torch.ceil(x)
+        z2 = torch.ceil(x.to(dipu))
         self.assertEqual(z1, z2.to(cpu))
 
-    def test_floor_inp(self):
+    def test_ceil_inp(self):
         dipu = torch.device("dipu")
         cpu = torch.device("cpu")
         x = torch.randn(4)
         y = x.clone().to(dipu)
-        torch.floor_(x)
-        torch.floor_(y)
+        torch.ceil_(x)
+        torch.ceil_(y)
         self.assertEqual(x, y.to(cpu))
 
 
