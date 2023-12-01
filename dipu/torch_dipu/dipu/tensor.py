518256aea9557ec83d8f55a99ff56bae4adae7d6
wyz5864 <109072365+wyz5864@users.noreply.github.com>
2023-12-01 14:59:53 +0800

    mock torch.cuda.XXXTensor (#462)
    * mock torch.cuda.XXXTensor
    
    * add newline at end of file
    
    * fix conflict
    
    * fix format
    
    * fix format
    
    * fix comment

--- dipu/torch_dipu/dipu/tensor.py
+++ dipu/torch_dipu/dipu/tensor.py
@@ -1,45 +1,21 @@
 # Copyright (c) 2023, DeepLink.
 import torch
+
 from .device import __diputype__
+from torch_dipu import _C, mockcuda
 
 
-# replace type exported from csrc/tensor/python_tensor.cpp
-# need replace torch._tensor_classes? (seems currently not need)
-class _MetaTensorType(type):
-    def __instancecheck__(cls, inst):
-        if isinstance(inst, torch.Tensor):
-          if inst.device.type == __diputype__ and inst.dtype == cls.dtype:
-            return True
-        return False
-
-class FloatTensor(metaclass=_MetaTensorType):
-   dtype = torch.float
-class DoubleTensor(metaclass=_MetaTensorType):
-   dtype= torch.float64
-class HalfTensor(metaclass=_MetaTensorType):
-   dtype= torch.float16
-
-class LongTensor(metaclass=_MetaTensorType):
-    dtype = torch.int64
-class IntTensor(metaclass=_MetaTensorType):
-    dtype = torch.int32
-class ShortTensor(metaclass=_MetaTensorType):
-    dtype = torch.int16
-class ByteTensor(metaclass=_MetaTensorType):
-    dtype = torch.uint8
-class CharTensor(metaclass=_MetaTensorType):
-    dtype = torch.int8
-class BoolTensor(metaclass=_MetaTensorType):
-    dtype = torch.bool
+_default_tensor_type = torch.FloatTensor
 
 
-_default_tensor_type = FloatTensor
+def __set_default_tensor_type(type = torch.FloatTensor):
+    print(" warnning!! dipu not support default tensor setting now!, this func is empty")
+    global _default_tensor_type
+    _default_tensor_type = type
 
-def __set_default_tensor_type(type = FloatTensor):
-  print(" warnning!! dipu not support default tensor setting now!, this func is empty")
-  global _default_tensor_type
-  _default_tensor_type = type
 
 # need enhance, seems change tensor define is need 
 def apply_tensor_type_patch():
-  torch.set_default_tensor_type = __set_default_tensor_type
+    torch.set_default_tensor_type = __set_default_tensor_type
+    if mockcuda:
+        _C._mockCudaTensor()
