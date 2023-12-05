f3a4579bf108ae825b46cf2acec682fa5184ddaf
POI-WX <wangxing2@sensetime.com>
2023-12-05 11:39:28 +0800

    improve maximum schema due to the case in the inference of internlm

--- dipu/tests/python/unittests/test_minimum_maximum.py
+++ dipu/tests/python/unittests/test_minimum_maximum.py
@@ -15,11 +15,27 @@ class TestMinimimMaximum(TestCase):
         r_cpu = torch.minimum(a.to(self.cpu), b.to(self.cpu))
         self.assertEqual(r_dipu.to(self.cpu), r_cpu)
 
+    def test_minimum_scalar(self):
+        # special test cases from the inference of internlm
+        a = torch.randn((3, 4))
+        b = torch.tensor(torch.finfo(a.dtype).max)
+        r_dipu = torch.minimum(a.to(self.dipu), b)
+        r_cpu = torch.minimum(a, b)
+        self.assertEqual(r_dipu.to(self.cpu), r_cpu)
+
     def test_maximum(self):
         a = torch.tensor((1, 2, -1))
         b = torch.tensor((3, 0, 4))
         r_dipu = torch.maximum(a.to(self.dipu), b.to(self.dipu))
         r_cpu = torch.maximum(a.to(self.cpu), b.to(self.cpu))
+        self.assertEqual(r_dipu.to(self.cpu), r_cpu)
+
+    def test_maximum_scalar(self):
+        # special test cases from the inference of internlm
+        a = torch.randn((3, 4))
+        b = torch.tensor(torch.finfo(a.dtype).min)
+        r_dipu = torch.maximum(a.to(self.dipu), b)
+        r_cpu = torch.maximum(a, b)
         self.assertEqual(r_dipu.to(self.cpu), r_cpu)
 
 
