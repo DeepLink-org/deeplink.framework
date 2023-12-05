b6806c1354b51d3ce589da0c54f03ae79ab3f836
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-12-05 13:56:50 +0800

    add remainder op wrapper and test case

--- dipu/tests/python/unittests/test_remainder.py
+++ dipu/tests/python/unittests/test_remainder.py
@@ -8,12 +8,20 @@ class TestRemainder(TestCase):
     def _test_remainder(self, a: torch.Tensor, b: float):
         a = a.cuda()
         out = torch.remainder(a, b)
-        gold = a - a.div(b, rounding_mode="floor") * b
-        self.assertEqual(out, gold)
+        #gold = a - a.div(b, rounding_mode="floor") * b
+        out_cpu = torch.remainder(a.cpu(), b)
+        self.assertEqual(out.cpu(), out_cpu)
 
     def test_remainder(self):
         self._test_remainder(torch.tensor([-3.0, -2, -1, 1, 2, 3]), 2)
         self._test_remainder(torch.tensor([1, 2, 3, 4, 5]), 1.5)
+
+    def test_remainder_self_is_scalar(self):
+        a = 2
+        b = torch.tensor([-3.0, -2, -1, 1, 2, 3])
+        out = torch.remainder(a, b.cuda())
+        out_cpu = torch.remainder(a, b.cpu())
+        self.assertEqual(out.cpu(), out_cpu)
 
 
 if __name__ == "__main__":
