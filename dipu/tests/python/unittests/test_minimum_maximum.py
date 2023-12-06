4a2ae4f26eb1c7687593a64a07b5c8e3e03a999c
POI-WX <wangxing2@sensetime.com>
2023-12-06 15:38:33 +0800

    fix bug according to comments

--- dipu/tests/python/unittests/test_minimum_maximum.py
+++ dipu/tests/python/unittests/test_minimum_maximum.py
@@ -19,9 +19,21 @@ class TestMinimimMaximum(TestCase):
         # special test cases from the inference of internlm
         a = torch.randn((3, 4))
         b = torch.tensor(torch.finfo(a.dtype).max)
-        r_dipu = torch.minimum(a.to(self.dipu), b)
+        # scalar on cpu
+        r_dipu1 = torch.minimum(a.to(self.dipu), b)
+        # scalar on device
+        r_dipu2 = torch.minimum(a.to(self.dipu), b.to(self.dipu))
         r_cpu = torch.minimum(a, b)
-        self.assertEqual(r_dipu.to(self.cpu), r_cpu)
+        self.assertEqual(r_dipu1.to(self.cpu), r_cpu)
+        self.assertEqual(r_dipu2.to(self.cpu), r_cpu)
+
+    def test_minimum_different_devices(self):
+        a = torch.tensor([1, -2, 3])
+        b = torch.tensor([4, 0, 2]).to(self.dipu)
+        with self.assertRaises(RuntimeError) as context:
+            torch.minimum(a, b)
+        self.assertIn(
+            'Expected all tensors to be on the same device', str(context.exception))
 
     def test_maximum(self):
         a = torch.tensor((1, 2, -1))
@@ -34,9 +46,21 @@ class TestMinimimMaximum(TestCase):
         # special test cases from the inference of internlm
         a = torch.randn((3, 4))
         b = torch.tensor(torch.finfo(a.dtype).min)
-        r_dipu = torch.maximum(a.to(self.dipu), b)
+        # scalar on cpu
+        r_dipu1 = torch.maximum(a.to(self.dipu), b)
+        # scalar on device
+        r_dipu2 = torch.maximum(a.to(self.dipu), b)
         r_cpu = torch.maximum(a, b)
-        self.assertEqual(r_dipu.to(self.cpu), r_cpu)
+        self.assertEqual(r_dipu1.to(self.cpu), r_cpu)
+        self.assertEqual(r_dipu2.to(self.cpu), r_cpu)
+
+    def test_maximum_different_devices(self):
+        a = torch.tensor([1, -2, 3])
+        b = torch.tensor([4, 0, 2]).to(self.dipu)
+        with self.assertRaises(RuntimeError) as context:
+            torch.maximum(a, b)
+        self.assertIn(
+            'Expected all tensors to be on the same device', str(context.exception))
 
 
 if __name__ == "__main__":
