73d7b953c8710c4d5d73c67634e15aa22435fb71
yaofengchen <67218893+yao-fengchen@users.noreply.github.com>
2023-12-06 14:23:56 +0800

    [dicp][tops] Support some ops for stable-diffusion. (#467)
    * Add sin, cos, erf, split.
    
    1. Generalize MakeTuple in tops_op.
    2. Generalize make_const in enflame codegen.
    3. Add sin, cos, erf, split for tops.
    4. Format Python code in dicp tops.
    
    * refine code
    
    * fix abs test path
    
    * clean up code of split.
    
    * adjust const op generation.
    
    * fix nullptr case in const generation.
    
    ---------
    
    Co-authored-by: jinminxi104 <jinminxi104@hotmail.com>
    Co-authored-by: Reinerzhou <1768552509@qq.com>

---
+++ dicp/test/op/test_sin.py
@@ -0,0 +1,40 @@
+import pytest
+from ..common.utils import (
+    torch,
+    dynamo,
+    parse_args,
+    compile_model,
+    get_device,
+    Size,
+    update_dynamo_config,
+)
+
+
+class OpModule(torch.nn.Module):
+    def forward(self, a):
+        res_default = torch.ops.aten.sin.default(a)
+        return res_default
+
+
+model = OpModule()
+args = parse_args()
+compiled_model = compile_model(model, args.backend, args.dynamic)
+
+
+class TestSin():
+    @pytest.mark.parametrize("dtype", [torch.float32])
+    @pytest.mark.parametrize("sizes", [Size((5,), (5, 3)), Size((3, 5), (5, 3)), Size((2, 3, 4), (2, 4))])
+    @pytest.mark.parametrize("compiled_model", compiled_model)
+    def test_torch_sin(self, sizes, dtype, compiled_model):
+        device = get_device()
+        size = sizes.dynamic if compiled_model.dynamic else sizes.static
+        input1 = torch.randn(size, dtype=dtype)
+
+        dicp_input1 = input1.to(device)
+
+        output = model(input1)
+        dynamo.reset()
+        update_dynamo_config(compiled_model.dynamic)
+        dicp_output = compiled_model.model(dicp_input1)
+
+        assert torch.allclose(output, dicp_output.cpu(), equal_nan=True)
