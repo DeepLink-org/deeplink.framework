cb81f879d9a882a351ba3c0027e091f12df9d365
POI-WX <131418410+POI-WX@users.noreply.github.com>
2023-12-06 18:25:42 +0800

    [DIPU] Wx/modify maximum schema due to the case in the inference of internlm (#494)
    * improve maximum schema due to the case in the inference of internlm
    
    * fix bug according to comments
    
    * fix bug

--- dipu/tests/python/unittests/test_minimum_maximum.py
+++ dipu/tests/python/unittests/test_minimum_maximum.py
@@ -15,12 +15,52 @@ class TestMinimimMaximum(TestCase):
         r_cpu = torch.minimum(a.to(self.cpu), b.to(self.cpu))
         self.assertEqual(r_dipu.to(self.cpu), r_cpu)
 
+    def test_minimum_scalar(self):
+        # special test cases from the inference of internlm
+        a = torch.randn((3, 4))
+        b = torch.tensor(torch.finfo(a.dtype).max)
+        # scalar on cpu
+        r_dipu1 = torch.minimum(a.to(self.dipu), b)
+        # scalar on device
+        r_dipu2 = torch.minimum(a.to(self.dipu), b.to(self.dipu))
+        r_cpu = torch.minimum(a, b)
+        self.assertEqual(r_dipu1.to(self.cpu), r_cpu)
+        self.assertEqual(r_dipu2.to(self.cpu), r_cpu)
+
+    def test_minimum_different_devices(self):
+        a = torch.tensor([1, -2, 3])
+        b = torch.tensor([4, 0, 2]).to(self.dipu)
+        with self.assertRaises(RuntimeError) as context:
+            torch.minimum(a, b)
+        self.assertIn(
+            'Expected all tensors to be on the same device', str(context.exception))
+
     def test_maximum(self):
         a = torch.tensor((1, 2, -1))
         b = torch.tensor((3, 0, 4))
         r_dipu = torch.maximum(a.to(self.dipu), b.to(self.dipu))
         r_cpu = torch.maximum(a.to(self.cpu), b.to(self.cpu))
         self.assertEqual(r_dipu.to(self.cpu), r_cpu)
+
+    def test_maximum_scalar(self):
+        # special test cases from the inference of internlm
+        a = torch.randn((3, 4))
+        b = torch.tensor(torch.finfo(a.dtype).min)
+        # scalar on cpu
+        r_dipu1 = torch.maximum(a.to(self.dipu), b)
+        # scalar on device
+        r_dipu2 = torch.maximum(a.to(self.dipu), b.to(self.dipu))
+        r_cpu = torch.maximum(a, b)
+        self.assertEqual(r_dipu1.to(self.cpu), r_cpu)
+        self.assertEqual(r_dipu2.to(self.cpu), r_cpu)
+
+    def test_maximum_different_devices(self):
+        a = torch.tensor([1, -2, 3])
+        b = torch.tensor([4, 0, 2]).to(self.dipu)
+        with self.assertRaises(RuntimeError) as context:
+            torch.maximum(a, b)
+        self.assertIn(
+            'Expected all tensors to be on the same device', str(context.exception))
 
 
 if __name__ == "__main__":
