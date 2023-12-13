dd511c5b6b97b4707fd69a8efd849781a305278c
yewentao <zhyanwentao@126.com>
2023-12-13 11:50:11 +0800

    update log.h

--- dipu/torch_dipu/csrc_dipu/utils/Log.h
+++ dipu/torch_dipu/csrc_dipu/utils/Log.h
@@ -8,13 +8,13 @@
 #define CONCAT(prefix, suffix) CONCAT_(prefix, suffix)
 #define MAKE_UNIQUE_VARIABLE_NAME(prefix) CONCAT(prefix##_, __LINE__)
 
-#define DIPU_LOG std::cout << __FILE__ << ":" << __LINE__ << "DIPU_LOG: "
+#define DIPU_LOG std::cout << __FILE__ << ":" << __LINE__ << " DIPU_LOG: "
 #define DIPU_LOG_ONCE                                                        \
   static auto& __attribute__((unused)) MAKE_UNIQUE_VARIABLE_NAME(__func__) = \
       DIPU_LOG
 
 #define DIPU_LOG_ERROR \
-  std::cerr << __FILE__ << ":" << __LINE__ << "DIPU_ERROR:  "
+  std::cerr << __FILE__ << ":" << __LINE__ << " DIPU_ERROR:  "
 #define DIPU_LOG_ERROR_ONCE                                                  \
   static auto& __attribute__((unused)) MAKE_UNIQUE_VARIABLE_NAME(__func__) = \
       DIPU_LOG_ERROR
