761a6644c1137c2767b7017355013fa9382e7aef
yaofengchen <fengchenyao@foxmail.com>
2023-12-19 10:38:19 +0000

    Fix test. 1.fix test config in tops 2.fix index test case for different backend

--- dicp/test/op/test_index.py
+++ dicp/test/op/test_index.py
@@ -22,6 +22,28 @@ compiled_model = compile_model(model, args.backend, args.dynamic)
 
 
 class TestIndex():
+    @pytest.mark.skipif(args.backend != "ascendgraph",
+                        reason="This is the test case for index in ascendgraph!")
+    @pytest.mark.parametrize("dtype", [torch.float32])
+    @pytest.mark.parametrize("sizes", [Size((5, 3), (5, 3)), Size((3, 5), (5, 3)), Size((4, 2), (4, 2))])
+    @pytest.mark.parametrize("compiled_model", compiled_model)
+    def test_torch_index_ascend(self, sizes, dtype, compiled_model):
+        device = get_device()
+        size = sizes.dynamic if compiled_model.dynamic else sizes.static
+        input1 = torch.randn(size, dtype=dtype)
+        input2 = [torch.tensor([0, 2])]
+
+        dicp_input1 = input1.to(device)
+        dicp_input2 = [torch.tensor([0, 2], device=device)]
+
+        output = model(input1, input2)
+        dynamo.reset()
+        update_dynamo_config(compiled_model.dynamic)
+        dicp_output = compiled_model.model(dicp_input1, dicp_input2)
+        assert torch.allclose(output, dicp_output.cpu(), equal_nan=True)
+
+    @pytest.mark.skipif(args.backend != "topsgraph",
+                        reason="This is the test case for index in topsgraph!")
     @pytest.mark.parametrize("dtype", [torch.float32])
     @pytest.mark.parametrize("sizes", [Size(((10, 15), (None, [0, 2],)), ((10, 15), (None, [0, 2],))),
                                        Size(((10, 15, 20), ([[1], [2]], [[3, 4]])),
@@ -29,7 +51,7 @@ class TestIndex():
                                        Size(((10, 15, 20, 25), (None, [[2, 2]], [[1, 3], [4, 5]], [[1], [2]])),
                                             ((10, 25), ([[1], [2]], [5])))])
     @pytest.mark.parametrize("compiled_model", compiled_model)
-    def test_torch_index(self, sizes, dtype, compiled_model):
+    def test_torch_index_tops(self, sizes, dtype, compiled_model):
         device = get_device()
         size = sizes.dynamic if compiled_model.dynamic else sizes.static
         input1 = torch.randn(size[0], dtype=dtype)
