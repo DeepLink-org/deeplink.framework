d547472140aec0a531b510218ef7d631f27bd5bb
wugeshui <106943115+wugeshui@users.noreply.github.com>
2023-12-21 11:41:52 +0800

    add sco

--- dipu/scripts/ci/ci_run_one_iter.py
+++ dipu/scripts/ci/ci_run_one_iter.py
@@ -11,9 +11,9 @@ import logging
 log_format = '%(asctime)s - %(levelname)s: %(message)s'
 logging.basicConfig(level=logging.INFO, format=log_format, datefmt='%Y-%m-%d %H:%M:%S')
 
-
+env = os.environ
 def run_cmd(cmd: str) -> None:
-    cp = sp.run(cmd, shell=True, encoding="utf-8")
+    cp = sp.run(cmd, shell=True, encoding="utf-8", env=env)
     if cp.returncode != 0:
         error = f"Some thing wrong has happened when running command [{cmd}]:{cp.stderr}"
         raise Exception(error)
@@ -95,16 +95,16 @@ def process_one_iter(log_file, clear_log, model_info: dict) -> None:
             cmd_run_one_iter = f"srun --job-name={job_name} --partition={partition}  --gres={gpu_requests} --cpus-per-task=5 --mem=16G --time=40 sh SMART/tools/one_iter_tool/run_one_iter.sh {train_path} {config_path} {work_dir} {opt_arg}"
             cmd_cp_one_iter = f"srun --job-name={job_name} --partition={partition}  --gres={gpu_requests} --cpus-per-task=5 --mem=16G --time=30 sh SMART/tools/one_iter_tool/compare_one_iter.sh {package_name}"
     elif device == 'sco':
+        current_path = os.getcwd()
         if (p2 == "stable_diffusion/stable-diffusion_ddim_denoisingunet_infer.py"):
-            cmd_run_one_iter = f"srun --job-name={job_name} bash mmagic/configs/stable_diffusion/stable-diffusion_ddim_denoisingunet_one_iter.sh"
-            cmd_cp_one_iter = ""
-        # For the inference of large language models, simply compare the inference results on the current device directly with the results generated on the GPU
+            cmd_run_one_iter_sco = f"""srun --job-name={job_name} bash -c "cd {current_path} && source scripts/ci/ci_one_iter.sh export_pythonpath_cuda {current_path}/mmlab_pack && source /mnt/cache/share/deeplinkci/github/dipu_env && cd mmlab_pack && bash {current_path}/mmlab_pack/mmagic/configs/stable_diffusion/stable-diffusion_ddim_denoisingunet_one_iter.sh" """
+            cmd_cp_one_iter_sco = ""
         elif ('infer' in p2 and 'infer' in p3):
-            cmd_run_one_iter = f"srun --job-name={job_name} python {train_path}"
-            cmd_cp_one_iter = ""
+            cmd_run_one_iter_sco = f"""srun --job-name={job_name} bash -c "cd {current_path} && source scripts/ci/ci_one_iter.sh export_pythonpath_cuda {current_path}/mmlab_pack && source /mnt/cache/share/deeplinkci/github/dipu_env && cd mmlab_pack && python {current_path}/mmlab_pack/{train_path}" """
+            cmd_cp_one_iter_sco = ""
         else:
-            cmd_run_one_iter = f"srun --job-name={job_name} bash SMART/tools/one_iter_tool/run_one_iter.sh {train_path} {config_path} {work_dir} {opt_arg}"
-            cmd_cp_one_iter = f"srun --job-name={job_name} bash SMART/tools/one_iter_tool/compare_one_iter.sh {package_name}"
+            cmd_run_one_iter_sco = f"""srun --job-name={job_name} bash -c "cd {current_path} && source scripts/ci/ci_one_iter.sh export_pythonpath_cuda {current_path}/mmlab_pack && source /mnt/cache/share/deeplinkci/github/dipu_env && cd mmlab_pack && bash {current_path}/mmlab_pack/SMART/tools/one_iter_tool/run_one_iter.sh {train_path} {config_path} {work_dir} {opt_arg}" """
+            cmd_cp_one_iter_sco = f"""srun --job-name={job_name} bash -c "cd {current_path} && source scripts/ci/ci_one_iter.sh export_pythonpath_cuda {current_path}/mmlab_pack && source /mnt/cache/share/deeplinkci/github/dipu_env && cd mmlab_pack && bash {current_path}/mmlab_pack/SMART/tools/one_iter_tool/compare_one_iter.sh {package_name}" """
     elif device == "camb" :
         # For the inference of large language models, simply compare the inference results on the current device directly with the results generated on the GPU
         if ('infer' in p2 and 'infer' in p3):
