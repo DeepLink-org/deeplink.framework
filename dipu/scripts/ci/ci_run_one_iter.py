fe5b2d232c5a1d7da1e0bf0f855835b3b4d6dab3
wugeshui <wugeshui@sensetime.com>
2024-01-02 10:38:45 +0800

    add sco ci

--- dipu/scripts/ci/ci_run_one_iter.py
+++ dipu/scripts/ci/ci_run_one_iter.py
@@ -81,10 +81,6 @@ def process_one_iter(log_file, clear_log, model_info: dict) -> None:
     rtol = precision.get('rtol', 1e-4)
     metric = precision.get('metric', 1e-2)
     logging.info(f'Using pricision: atol-{atol}, rtol-{rtol}, metric-{metric}')
-    env_vars = os.environ
-    print(env_vars)
-    for key, value in env_vars.items():
-    print(f"{key}: {value}")
     if device == 'cuda':
         if (p2 == "stable_diffusion/stable-diffusion_ddim_denoisingunet_infer.py"):
             cmd_run_one_iter = f"srun --job-name={job_name} --partition={partition}  --gres={gpu_requests} --cpus-per-task=5 --mem=16G --time=40 sh mmagic/configs/stable_diffusion/stable-diffusion_ddim_denoisingunet_one_iter.sh"
@@ -98,16 +94,15 @@ def process_one_iter(log_file, clear_log, model_info: dict) -> None:
             cmd_cp_one_iter = f"srun --job-name={job_name} --partition={partition}  --gres={gpu_requests} --cpus-per-task=5 --mem=16G --time=30 sh SMART/tools/one_iter_tool/compare_one_iter.sh {package_name}"
     elif device == 'sco':
         current_path = os.getcwd()
-        parent_directory = os.path.dirname(current_path)
         if (p2 == "stable_diffusion/stable-diffusion_ddim_denoisingunet_infer.py"):
-            cmd_run_one_iter = f"""srun --job-name={job_name} bash -c "cd {parent_directory} && source scripts/ci/ci_one_iter.sh export_pythonpath_cuda {current_path} && source /mnt/cache/share/deeplinkci/github/dipu_env && cd mmlab_pack && bash {current_path}/mmagic/configs/stable_diffusion/stable-diffusion_ddim_denoisingunet_one_iter.sh" """
+            cmd_run_one_iter = f"""srun --job-name={job_name} bash -c "cd {current_path} && source environment_exported && source /mnt/cache/share/deeplinkci/github/dipu_env && bash {current_path}/mmagic/configs/stable_diffusion/stable-diffusion_ddim_denoisingunet_one_iter.sh" """
             cmd_cp_one_iter = ""
         elif ('infer' in p2 and 'infer' in p3):
-            cmd_run_one_iter = f"""srun --job-name={job_name} bash -c "cd {parent_directory} && source scripts/ci/ci_one_iter.sh export_pythonpath_cuda {current_path} && source /mnt/cache/share/deeplinkci/github/dipu_env && cd mmlab_pack && python {current_path}/{train_path}" """
+            cmd_run_one_iter = f"""srun --job-name={job_name} bash -c "cd {current_path} && source environment_exported && source /mnt/cache/share/deeplinkci/github/dipu_env && python {current_path}/{train_path}" """
             cmd_cp_one_iter = ""
         else:
-            cmd_run_one_iter = f"""srun --job-name={job_name} bash -c "cd {parent_directory} && source scripts/ci/ci_one_iter.sh export_pythonpath_cuda {current_path} && source /mnt/cache/share/deeplinkci/github/dipu_env && cd mmlab_pack && bash {current_path}/SMART/tools/one_iter_tool/run_one_iter.sh {train_path} {config_path} {work_dir} {opt_arg}" """
-            cmd_cp_one_iter = f"""srun --job-name={job_name} bash -c "cd {parent_directory} && source scripts/ci/ci_one_iter.sh export_pythonpath_cuda {current_path} && source /mnt/cache/share/deeplinkci/github/dipu_env && cd mmlab_pack && bash {current_path}/SMART/tools/one_iter_tool/compare_one_iter.sh {package_name}" """
+            cmd_run_one_iter = f"""srun --job-name={job_name} bash -c "cd {current_path} && source environment_exported && source /mnt/cache/share/deeplinkci/github/dipu_env && bash {current_path}/SMART/tools/one_iter_tool/run_one_iter.sh {train_path} {config_path} {work_dir} {opt_arg}" """
+            cmd_cp_one_iter = f"""srun --job-name={job_name} bash -c "cd {current_path} && source environment_exported && source /mnt/cache/share/deeplinkci/github/dipu_env && bash {current_path}/SMART/tools/one_iter_tool/compare_one_iter.sh {package_name}" """
     elif device == "camb" :
         # For the inference of large language models, simply compare the inference results on the current device directly with the results generated on the GPU
         if ('infer' in p2 and 'infer' in p3):
