9444bbf57935e7941e27a71f6326b6092854da30
jingguo-st <fujingguo@sensetime.com>
2024-01-04 15:27:30 +0800

    add test case for ne

---
+++ dipu/tests/python/individual_scripts/test_ne.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2023, DeepLink.
+import torch
+import torch_dipu
+from torch_dipu.testing._internal.common_utils import TestCase, run_tests
+
+
+class TestLogicalNe(TestCase):
+    def setUp(self):
+        self.device = torch.device("dipu")
+
+    def test_ne(self):
+        x = torch.randn(5, 5)
+        o = torch.randn(5, 5)
+
+        res_on_cpu = torch.ne(x, o)
+        res_on_npu = torch.ne(x.to(self.device), o.to(self.device))
+        res_on_cpu = res_on_cpu
+        self.assertEqual(res_on_cpu, res_on_npu.cpu())
+
+        s = torch.randn(1)
+        res_on_cpu = torch.ne(x, s)
+        res_on_npu = torch.ne(x.to(self.device), s.to(self.device))
+        self.assertEqual(res_on_cpu, res_on_npu.cpu())
+
+        res_on_cpu = torch.ne(s, x)
+        res_on_npu = torch.ne(s.to(self.device), x.to(self.device))
+        self.assertEqual(res_on_cpu, res_on_npu.cpu())
+
+if __name__ == "__main__":
+    run_tests()
