5f8e0d317e1a58113abb571921cf0b9f3fa79acd
zhangqiu1994 <zhangqiu1994@outlook.com>
2024-01-05 10:35:47 +0800

    add test for normal_scalar_scalar

--- dipu/tests/python/unittests/test_normal.py
+++ dipu/tests/python/unittests/test_normal.py
@@ -12,6 +12,15 @@ class TestNormal(TestCase):
         self.assertEqual(x.mean().item(), 0.0, prec=0.01)
         self.assertEqual(x.std().item(), 1.0, prec=0.01)
     
+    def test_normal_scalar_scalar(self):
+        dipu = torch.device("dipu")
+        mean_val = 0
+        std_val = 1
+        out_size = [1000000]
+        out_tensor = torch.normal(mean=mean_val, std=std_val, size=out_size, device=dipu)
+        self.assertEqual(out_tensor.mean().item(), mean_val, prec=0.01)
+        self.assertEqual(out_tensor.std().item(), std_val, prec=0.01)
+        
     def test_normal_tensor_tensor(self):
         dipu = torch.device("dipu")
         mean_val = 0
