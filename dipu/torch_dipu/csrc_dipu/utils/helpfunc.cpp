DeepLink-org/deeplink.framework.dev

support format cast

dipu/torch_dipu/csrc_dipu/utils/helpfunc.cpp
@@ -1,6 +1,10 @@
 // Copyright (c) 2023, DeepLink.
 #include "./helpfunc.hpp"
 
+#include <diopi/diopirt.h>
+#include <diopi/functions_ext.h>
+
+#include "csrc_dipu/diopirt/diopirt_impl.h"
 #ifndef WIN32
 #include <mutex>
 #include <pthread.h>
@@ -10,6 +14,29 @@ namespace dipu {
 bool isDeviceTensor(const at::Tensor& tensor) {
   // same as tensor.device().type()
   return tensor.unsafeGetTensorImpl()->device_type() == dipu::DIPU_DEVICE_TYPE;
+}
+
+at::Tensor format_cast(at::Tensor& tensor, diopiMemoryFormat_t target_format) {
+  TORCH_CHECK(isDeviceTensor(tensor), "only device tensor support this api.");
+  TORCH_CHECK(::diopiFormatCast, "diopi not support this api.");
+  ::diopiTensorHandle_t in = dipu::diopi_helper::toDiopiTensorHandle(tensor);
+  ::diopiContext context(dipu::getCurrentDIPUStream().rawstream());
+  at::Tensor t = at::empty({0}, tensor.options());
+  ::diopiTensorHandle_t out = dipu::diopi_helper::toDiopiTensorHandle(t);
+  ::diopiFormatCast(&context, out, in, target_format);
+  return t;
+}
+
+diopiMemoryFormat_t get_format(const at::Tensor& tensor) {
+  if (!isDeviceTensor(tensor)) {
+    return diopiMemoryFormat_t::Undefined;
+  }
+  TORCH_CHECK(::diopiGetFormat, "diopi not support this api.");
+  ::diopiContext context(dipu::getCurrentDIPUStream().rawstream());
+  ::diopiConstTensorHandle_t input = dipu::diopi_helper::toDiopiTensorHandle(tensor);
+  diopiMemoryFormat_t format;
+  ::diopiGetFormat(&context, input, &format);
+  return format;
 }
 
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)

dipu/torch_dipu/dipu/__init__.py
@@ -9,8 +9,10 @@ from .memory import *
 from .streams import *
 from .tensor import *
 from .storages import *
+from .utils import format_cast
 from . import amp
 import torch_dipu
+from torch_dipu._C import DIOPIMemoryFormat
 
 _is_in_bad_fork = getattr(torch_dipu._C, "_is_in_bad_fork", lambda: False)
 
@@ -38,6 +40,8 @@ __all__ = [
     "reset_peak_memory_stats", "empty_cache", "memory_allocated", "memory_reserved", "max_memory_allocated", "max_memory_reserved",
     "mem_get_info", # "caching_allocator_alloc", "caching_allocator_delete", "memory_summary", "memory_stats"
 
+    # custom api
+    "format_cast", "DIOPIMemoryFormat"
     # not support mock cuda_graph now
 
 ]

dipu/torch_dipu/dipu/utils.py
@@ -4,12 +4,17 @@ import os
 import traceback
 import threading
 from multiprocessing.util import register_after_fork as _register_after_fork
+import torch
+from torch_dipu import _C
 
 _initialized = True
 _queued_calls = []  # don't invoke these until initialization occurs
 _in_bad_fork = False  # this global is also used in torch.manual_seed
 _original_pid = False
 
+
+def format_cast(tensor:torch.Tensor, format:_C.DIOPIMemoryFormat) -> torch.Tensor:
+    return _C.format_cast(tensor, format)
 
 def is_initialized():
     r"""Returns whether PyTorch's dipu state has been initialized."""

DeepLink-org/deeplink.framework

dipu/torch_dipu/csrc_dipu/utils/helpfunc.cpp
@@ -1,6 +1,10 @@
 // Copyright (c) 2023, DeepLink.
 #include "./helpfunc.hpp"
 
+#include <diopi/diopirt.h>
+#include <diopi/functions_ext.h>
+
+#include "csrc_dipu/diopirt/diopirt_impl.h"
 #ifndef WIN32
 #include <mutex>
 #include <pthread.h>
@@ -10,6 +14,28 @@ namespace dipu {
 bool isDeviceTensor(const at::Tensor& tensor) {
   // same as tensor.device().type()
   return tensor.unsafeGetTensorImpl()->device_type() == dipu::DIPU_DEVICE_TYPE;
+}
+
+at::Tensor format_cast(at::Tensor tensor, diopiCustomFormat_t target_format) {
+  TORCH_CHECK(isDeviceTensor(tensor), "only device tensor support this api.");
+  TORCH_CHECK(::diopiFormatCast, "diopi not support this api.");
+  ::diopiTensorHandle_t in = dipu::diopi_helper::toDiopiTensorHandle(tensor);
+  ::diopiContext context(dipu::getCurrentDIPUStream().rawstream());
+  ::diopiTensorHandle_t out = nullptr;
+  ::diopiFormatCast(&context, &out, in, target_format);
+  return *(reinterpret_cast<at::Tensor*>(out));
+}
+
+diopiCustomFormat_t get_format(at::Tensor tensor) {
+  if (!isDeviceTensor(tensor)) {
+    return diopiCustomFormat_t::Undefined;
+  }
+  TORCH_CHECK(::diopiGetFormat, "diopi not support this api.");
+  ::diopiContext context(dipu::getCurrentDIPUStream().rawstream());
+  ::diopiTensorHandle_t input = dipu::diopi_helper::toDiopiTensorHandle(tensor);
+  diopiCustomFormat_t format;
+  ::diopiGetFormat(&context, input, &format);
+  return format;
 }
 
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)

dipu/torch_dipu/dipu/__init__.py
@@ -9,8 +9,10 @@ from .memory import *
 from .streams import *
 from .tensor import *
 from .storages import *
+from .utils import format_cast
 from . import amp
 import torch_dipu
+from torch_dipu._C import DIOPICustomFormat
 
 _is_in_bad_fork = getattr(torch_dipu._C, "_is_in_bad_fork", lambda: False)
 
@@ -38,6 +40,8 @@ __all__ = [
     "reset_peak_memory_stats", "empty_cache", "memory_allocated", "memory_reserved", "max_memory_allocated", "max_memory_reserved",
     "mem_get_info", # "caching_allocator_alloc", "caching_allocator_delete", "memory_summary", "memory_stats"
 
+    # custom api
+    "format_cast", "DIOPICustomFormat"
     # not support mock cuda_graph now
 
 ]

dipu/torch_dipu/dipu/utils.py
@@ -4,12 +4,17 @@ import os
 import traceback
 import threading
 from multiprocessing.util import register_after_fork as _register_after_fork
+import torch
+from torch_dipu import _C
 
 _initialized = True
 _queued_calls = []  # don't invoke these until initialization occurs
 _in_bad_fork = False  # this global is also used in torch.manual_seed
 _original_pid = False
 
+
+def format_cast(tensor:torch.Tensor, format:_C.DIOPICustomFormat) -> torch.Tensor:
+    return _C.format_cast(tensor, format)
 
 def is_initialized():
     r"""Returns whether PyTorch's dipu state has been initialized."""