354e5beba023f19c84c3f72e175b800647273985
LAPTOP-7KKLK9PN\Kevin <2498530516@qq.com>
2024-01-11 08:18:37 +0000

    remove inference of 'BroadcastTo' to adpat to the modification for 'lt' in coversion.py

--- dicp/dicp/vendor/AscendGraph/ascend_op.py
+++ dicp/dicp/vendor/AscendGraph/ascend_op.py
@@ -43,6 +43,11 @@ class BroadcastTo(Operator):
         super().__init__("BroadcastTo")
 
     def infer_result(self, x, shape):
+        # current implementation of conversion.py doesn't require "BroadcastTo" to infer_shape_dtype
+        # for "BroadcastTo" now is only used as an intermediate node in converted graph, 
+        # and the original input is enough to infer final node's shape and dtype
+        return x
+        '''
         x, x_shape, x_dim, x_dtype = get_fake_tensor_meta_val(x)
         if isinstance(shape, torch._subclasses.fake_tensor.FakeTensor): # case1: shape is a fakeTensor, like conversion for 'scatter' and 'where'
             shape, shape_shape, shape_dim, shape_dtype = get_fake_tensor_meta_val(shape)
@@ -57,6 +62,7 @@ class BroadcastTo(Operator):
             self.__class__.__name__ + "can't broadcast x to specified shape!"
         )
         return torch.empty(shape, dtype=x_dtype, memory_format=get_memory_format(x))
+        '''
 
 
 class Range(Operator):
