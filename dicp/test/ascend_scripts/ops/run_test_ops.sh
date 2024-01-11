ece5e9646fc2d17879f92a1fa9117c9950acba90
LAPTOP-7KKLK9PN\Kevin <2498530516@qq.com>
2024-01-11 05:48:18 +0000

    ci test failure case

--- dicp/test/ascend_scripts/ops/run_test_ops.sh
+++ dicp/test/ascend_scripts/ops/run_test_ops.sh
@@ -17,15 +17,21 @@ CONFIG_DYNAMIC=${CONFIG_DIR}/dynamic.ini
 export TEST_DICP_INFER=1
 cd ${TEST_OP_DIR}
 if [ ${DYNAMIC} == false ]; then
-    pytest -c ${CONFIG_STATIC} --backend ${BACKEND} --dynamic ${DYNAMIC}
+    output=$(pytest -c ${CONFIG_STATIC} --backend ${BACKEND} --dynamic ${DYNAMIC}  | tee /dev/tty )
 elif [ ${DYNAMIC} == true ]; then
-    pytest -c ${CONFIG_DYNAMIC} --backend ${BACKEND} --dynamic ${DYNAMIC}
+    output=$(pytest -c ${CONFIG_DYNAMIC} --backend ${BACKEND} --dynamic ${DYNAMIC}  | tee /dev/tty)
 elif [ ${DYNAMIC} == all ]; then
-    pytest -c ${CONFIG_STATIC} --backend ${BACKEND} --dynamic false
-    pytest -c ${CONFIG_DYNAMIC} --backend ${BACKEND} --dynamic true
+    output=$(pytest -c ${CONFIG_STATIC} --backend ${BACKEND} --dynamic false  | tee /dev/tty)
+    output+="\n"
+    output=$(pytest -c ${CONFIG_DYNAMIC} --backend ${BACKEND} --dynamic true  | tee /dev/tty)
 else
     echo "DYNAMIC should in (true, false, all)" >&2
     unset TEST_DICP_INFER
     exit 1
 fi
 unset TEST_DICP_INFER
+
+if echo "$output" | grep -q "FAILED"; then
+    echo "ERROR! Not all pytest tests passed!" >&2
+    exit 1
+fi
