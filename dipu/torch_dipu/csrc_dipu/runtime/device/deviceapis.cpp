df6bb3626746757a31ff6e117acbcedfccaa8a89
Lingjie Li <lljbash@gmail.com>
2024-01-12 17:04:03 +0800

    fix extern const var

--- dipu/torch_dipu/csrc_dipu/runtime/device/deviceapis.cpp
+++ dipu/torch_dipu/csrc_dipu/runtime/device/deviceapis.cpp
@@ -1,14 +1,15 @@
 // Copyright (c) 2023, DeepLink.
 
-#include "basedef.h"
 #include "csrc_dipu/utils/vender_helper.hpp"
+
+#include "basedef.h"
 
 namespace dipu {
 
 #ifndef DIPU_VENDOR_NAME
 #error "DIPU_VENDOR_NAME must be defined"
 #else
-const devapis::VendorDeviceType kDipuVendorDeviceType =
+extern const devapis::VendorDeviceType kDipuVendorDeviceType =
     VendorNameToDeviceType(DIPU_STRINGIFY_AFTER_EXPANSION(DIPU_VENDOR_NAME));
 #endif
 
