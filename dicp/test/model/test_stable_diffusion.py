517e0e28625a73bce19c2ab2e360b8cb31726c4d
Pan Daoxin <pandaoxin@pjlab.org.cn>
2024-01-15 03:19:28 +0000

    Fix unit test.

--- dicp/test/model/test_stable_diffusion.py
+++ dicp/test/model/test_stable_diffusion.py
@@ -27,9 +27,25 @@ class TestStableDiffusion():
         utils.update_dynamo_config(dynamic=dynamic)
         torch_dipu.dipu.set_device(device)
 
+        # pytorch monkey patch for ascendgraph backend
+        if backend == "ascendgraph":
+            import importlib
+            tmp_variable_torch_module = importlib.import_module("torch._dynamo.variables.torch")
+            tmp_torch_variable = getattr(tmp_variable_torch_module, "TorchVariable")
+            origin_torch_variable_python_type = getattr(tmp_torch_variable, "python_type")
+            def new_torch_variable_python_type(self):
+                if isinstance(self.value, torch.device):
+                    return type(self.value)
+                else:
+                    return origin_torch_variable_python_type(self)
+            setattr(tmp_torch_variable, "python_type", new_torch_variable_python_type)
+
         dicp_pipe = StableDiffusionPipeline.from_pretrained(model_path).to(device)
         dicp_pipe.text_encoder = torch.compile(dicp_pipe.text_encoder, backend=backend)
         dicp_pipe.unet = torch.compile(dicp_pipe.unet, backend=backend)
+        if backend == "ascendgraph":
+            dicp_pipe.vae.decoder = torch.compile(dicp_pipe.vae.decoder, backend=backend)
+
         dicp_image = dicp_pipe(prompt, num_inference_steps=num_inference_steps).images[0]
         if backend == "ascendgraph":
             standard_output = torch.load("stable_diffusion/ascendgraph_output.pt")
