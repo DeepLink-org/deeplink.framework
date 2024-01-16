c047c674db78ea6a37cf1d57c2d72b86870cfa27
Lingjie Li <lljbash@gmail.com>
2024-01-16 11:02:29 +0800

    [dipu] add black CI check & format script

--- run_format.sh
+++ run_format.sh
@@ -6,3 +6,9 @@ git ls-files |\
   grep -E '^.+\.(c|h|cpp|cc|cxx|hpp|hh|hxx|cu|cuh)$' |\
   grep -Ev "$CLANG_FORMAT_EXCLUDE_REGEX" |\
   xargs clang-format -i --style=file
+
+# format all Python files in current git repository with black
+# now only for dipu
+git ls-files dipu |\
+  grep -E '^.+\.py$' |\
+  xargs black
