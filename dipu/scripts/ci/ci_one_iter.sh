e8b1dec472f487635545db4e288d88820373930e
brianlcy123 <brianlcy123@gmail.com>
2024-01-17 18:46:17 +0800

    [KUNLUNXIN] Complete CI pipeline for dpu (#622)
    * [kunlunxin]support multi-xpu training
    
    * [kunlunxin]fix broadcast impl
    
    * forward DIOPI to latest version
    
    * resolve submodule conflicts
    
    * resolve merge conflicts
    
    * fix linter error
    
    * fix linter error
    
    * [kunlunxin] impl CI pipeline for dipu
    
    * [kunlunxin] update ci error message
    
    * [KUNLUNXIN]remove unused code
    
    * [KUNLUNXIN] fix dipu ci
    
    * [KUNLUNXIN] fix ci data path
    
    * [KUNLUNXIN] fix dipu ci
    
    * [KUNLUNXIN]update DIOPI version

--- dipu/scripts/ci/ci_one_iter.sh
+++ dipu/scripts/ci/ci_one_iter.sh
@@ -86,6 +86,10 @@ function build_needed_repo_ascend() {
     cd ..
 }
 
+function build_needed_repo_kunlunxin() {
+    echo "skip"
+}
+
 
 function export_repo_pythonpath(){
     basic_path="$2"
@@ -101,8 +105,10 @@ function export_repo_pythonpath(){
         export PYTHONPATH=${basic_path}/mmagic/mmagic/models/editors/stable_diffusion:$PYTHONPATH
     elif [ "$1" = "ascend" ]; then
         echo "Executing ASCEND operation in pythonpath..."
+    elif [ "$1" = "kunlunxin" ]; then
+        echo "Executing KUNLUNXIN operation in pythonpath..."
     else
-        echo "Invalid parameter. Please specify 'cuda', 'camb' or 'ascend'."
+        echo "Invalid parameter. Please specify 'cuda', 'camb' 'ascend' or 'kunlunxin'."
         exit 1
     fi
     export PYTHONPATH=${basic_path}:$PYTHONPATH
@@ -197,9 +203,14 @@ function build_dataset(){
         ln -s /mnt/cache/share/datasets/mmdet/pretrain/darknet53-a628ea1b.pth data/darknet53-a628ea1b.pth
         ln -s /mnt/cache/share/datasets/mmpose/pretrain/hrnet_w32-36af842e.pth data/hrnet_w32-36af842e.pth
         ln -s /mnt/cache/share/datasets/pretrain/mmcv/resnet50_v1c-2cccc1ad.pth data/resnet50_v1c-2cccc1ad.pth
+    elif [ "$1" = "kunlunxin" ]; then
+        echo "Executing KUNLUNXIN operation in build dataset..."
+        rm -rf data
+        mkdir data
+        ln -s /mnt/cache/share/datasets/imagenet data/imagenet
 
     else
-        echo "Invalid parameter. Please specify 'cuda' 'camb' or 'ascend'."
+        echo "Invalid parameter. Please specify 'cuda' 'camb' 'ascend' or 'kunlunxin'."
         exit 1
     fi
 }
@@ -220,12 +231,17 @@ case $1 in
     build_ascend910b)
         build_needed_repo_ascend
         build_dataset ascend910b;;
+    build_kunlunxin)
+        build_needed_repo_kunlunxin
+        build_dataset kunlunxin;;
     export_pythonpath_camb)
         export_repo_pythonpath camb $2;;
     export_pythonpath_cuda)
         export_repo_pythonpath cuda $2;;
     export_pythonpath_ascend)
         export_repo_pythonpath ascend $2;;
+    export_pythonpath_kunlunxin)
+        export_repo_pythonpath kunlunxin $2;;
     *)
         echo -e "[ERROR] Incorrect option:" $1;
 esac
