70a23d2d529d8c036e7f56810853c10f14c8fa55
yaofengchen <fengchenyao@foxmail.com>
2024-01-18 00:59:18 +0000

    Fix some ops' custom_fallback error.

--- dicp/scripts/ci/ascend/test_env.sh
+++ dicp/scripts/ci/ascend/test_env.sh
@@ -4,3 +4,4 @@ LLAMA_MODEL_DIR=$1
 
 export DIPU_MOCK_CUDA=false
 export LLAMA_MODEL_DIR=$1
+export DIPU_KEEP_TORCHOP_DEFAULT_IMPL_OPS="rsqrt.out,mm,linear,_softmax.out"
--- dicp/scripts/ci/tops/ci_tops_test_env.sh
+++ dicp/scripts/ci/tops/ci_tops_test_env.sh
@@ -8,3 +8,4 @@ export DIPU_MOCK_CUDA=False
 # export LLAMA_MODEL_DIR=$1
 # export LLAMA_FINETUNE_DIR=$2
 # export STABLE_DIFFUSION_MODEL_DIR=$3
+export DIPU_KEEP_TORCHOP_DEFAULT_IMPL_OPS="rsqrt.out,_softmax.out"
