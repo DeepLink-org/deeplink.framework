a7c0173155cb960555dd901354c3e0568c7dd8ac
wugeshui <wugeshui@sensetime.com>
2024-01-19 10:56:00 +0800

    add pytorch 2.1.1

--- dipu/scripts/ci/camb/ci_camb_env.sh
+++ dipu/scripts/ci/camb/ci_camb_env.sh
@@ -9,14 +9,16 @@ export NEUWARE_HOME=/usr/local/neuware
 export CC=${GCC_ROOT}/bin/gcc
 export CXX=${GCC_ROOT}/bin/g++
 
-
 export DIOPI_ROOT=$(pwd)/third_party/DIOPI/impl/lib/
 export DIPU_ROOT=$(pwd)/torch_dipu
 export LD_LIBRARY_PATH=$DIPU_ROOT:$LD_LIBRARY_PATH
-export PYTHONPATH=${PLATFORM}/dep/DIOPI_pytorch/pytorch2.0:${PYTHONPATH}
+if [ -n "$1" ];then
+  export PYTHONPATH=${PLATFORM}/dep/DIOPI_pytorch/pytorch$1:${PYTHONPATH}
+else
+  export PYTHONPATH=${PLATFORM}/dep/DIOPI_pytorch/pytorch2.0:${PYTHONPATH}
+fi
 export PATH=${GCC_ROOT}/bin:${CONDA_ROOT}/envs/dipu_poc/bin:${CONDA_ROOT}/bin:${PATH}
 export LD_PRELOAD=${GCC_ROOT}/lib64/libstdc++.so.6
-
 
 export NEUWARE_ROOT_DIR=${NEUWARE_HOME}
 export VENDOR_INCLUDE_DIRS=${NEUWARE_HOME}/include
@@ -24,8 +26,6 @@ export DIOPI_PATH=$(pwd)/third_party/DIOPI/proto
 export DIPU_PATH=${DIPU_ROOT}
 
 #export MLU_INVOKE_BLOCKING=1
-
-
 #export DIPU_DEBUG_ALLOCATOR=15
 export DIPU_DEVICE_MEMCACHING_ALGORITHM=BS
 export DIPU_HOST_MEMCACHING_ALGORITHM=BS
