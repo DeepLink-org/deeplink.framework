b906e2f0fe080702ab3c0b95dfbccc74daa57e4d
liwenjian-sensetime <109193776+liwenjian-sensetime@users.noreply.github.com>
2024-01-25 15:30:12 +0800

    add kunlunxin coverage (#670)
    add kunlunxin coverage

--- dipu/torch_dipu/csrc_dipu/aten/ops/CustomFallbackFunctions.hpp
+++ dipu/torch_dipu/csrc_dipu/aten/ops/CustomFallbackFunctions.hpp
@@ -449,18 +449,18 @@ static at::Tensor custom_fallback_dipu_linear(
   return out;
 }
 
-static at::Tensor custom_fallback_dipu_rsqrt_out(const at::Tensor& self,
-                                                 at::Tensor& out) {
+static at::Tensor& custom_fallback_dipu_rsqrt_out(const at::Tensor& self,
+                                                  at::Tensor& out) {
   auto self_cpu = to_cpu_with_half_to_float(self);
   auto out_cpu = at::rsqrt(self_cpu);
   out.copy_(out_cpu);
   return out;
 }
 
-static at::Tensor custom_fallback_dipu__softmax_out(const at::Tensor& self,
-                                                    int64_t dim,
-                                                    bool half_to_float,
-                                                    at::Tensor& out) {
+static at::Tensor& custom_fallback_dipu__softmax_out(const at::Tensor& self,
+                                                     int64_t dim,
+                                                     bool half_to_float,
+                                                     at::Tensor& out) {
   auto self_cpu = to_cpu_with_half_to_float(self);
   auto out_cpu = at::softmax(self_cpu, dim);
   out.copy_(out_cpu);
