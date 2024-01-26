cadd6898ec0df763cbb62d98bbca44ceca13f6ca
wiryls <7984500+wiryls@users.noreply.github.com>
2024-01-26 19:29:33 +0800

    fix: put proxy_on inside script

--- dipu/scripts/ci/nv/ci_nv_tidy.sh
+++ dipu/scripts/ci/nv/ci_nv_tidy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-# Try finding clangd and libstdc++.so.6 on sco.
+# Try finding clangd and libstdc++.so.6 on SCO.
 # Note 1: ":+:" is used to handle unbound variable.
 # Note 2: the following code might be outdated.
+[ -f /mnt/cache/share/deeplinkci/github/proxy_on ] &&
+    source /mnt/cache/share/deeplinkci/github/proxy_on
 [ -d /mnt/cache/share/platform/dep/clang-16/bin ] &&
     export PATH=/mnt/cache/share/platform/dep/clang-16/bin${PATH:+:$PATH}
-[ -d /mnt/cache/share/platform/env/miniconda3.10/envs/pt2.0_diopi/lib ] &&
-    export LD_LIBRARY_PATH=/mnt/cache/share/platform/env/miniconda3.10/envs/pt2.0_diopi/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 
 # Required tools.
 [ -x "$(command -v git)" ] || { echo "::error::Missing git tool" && exit 1; }
 [ -x "$(command -v clangd)" ] || { echo "::error::Missing clangd tool" && exit 1; }
 
 # Get current folder.
-self=$(dirname "$(realpath -s "$0")")
+self=$(dirname "$(realpath -s "${BASH_SOURCE[0]}")")
 repo=$(cd "$self" && git rev-parse --show-toplevel)
 
 # Download clangd-tidy scripts.
@@ -24,5 +24,5 @@ repo=$(cd "$self" && git rev-parse --show-toplevel)
 # Forward srun commands.
 # e.g. you can use "bash scripts/ci/nv/ci_nv_tidy.sh srun -p pat_rd" to run tidy.
 (cd "$repo/dipu" &&
-    $@ "$self/clangd-tidy/clangd-tidy" -j4 -v \
-        $(find torch_dipu ! -path '*/vendor/*' ! -name 'AutoGenedKernels.cpp' \( -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \)))
+    find torch_dipu ! -path '*/vendor/*' ! -name 'AutoGenedKernels.cpp' \( -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) |
+    xargs "$self/clangd-tidy/clangd-tidy" -j4)
