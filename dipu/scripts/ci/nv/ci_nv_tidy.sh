b6faa372db477e1cf3f105cbd4139e514bb8624c
wiryls <7984500+wiryls@users.noreply.github.com>
2024-01-29 10:37:52 +0800

    doc: update comments and try 17

--- dipu/scripts/ci/nv/ci_nv_tidy.sh
+++ dipu/scripts/ci/nv/ci_nv_tidy.sh
@@ -3,11 +3,10 @@ set -euo pipefail
 
 # Try finding clangd and libstdc++.so.6 on SCO.
 # Note 1: ":+:" is used to handle unbound variable.
-# Note 2: the following code might be outdated.
 [ -f /mnt/cache/share/deeplinkci/github/proxy_on ] &&
     source /mnt/cache/share/deeplinkci/github/proxy_on
-[ -d /mnt/cache/share/platform/dep/clang-16/bin ] &&
-    export PATH=/mnt/cache/share/platform/dep/clang-16/bin${PATH:+:$PATH}
+[ -d /mnt/cache/share/platform/dep/clang-17/bin ] &&
+    export PATH=/mnt/cache/share/platform/dep/clang-17/bin${PATH:+:$PATH}
 
 # Required tools.
 [ -x "$(command -v git)" ] || { echo "::error::Missing git tool" && exit 1; }
@@ -21,8 +20,7 @@ repo=$(cd "$self" && git rev-parse --show-toplevel)
 [ -d "$self/clangd-tidy" ] ||
     git -c advice.detachedHead=false clone --depth 1 -b v0.1.0 https://github.com/lljbash/clangd-tidy.git "$self/clangd-tidy"
 
-# Forward srun commands.
-# e.g. you can use "bash scripts/ci/nv/ci_nv_tidy.sh srun -p pat_rd" to run tidy.
+# Collect source files and run tidy.
 (cd "$repo/dipu" &&
     find torch_dipu ! -path '*/vendor/*' ! -name 'AutoGenedKernels.cpp' \( -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) |
     xargs "$self/clangd-tidy/clangd-tidy" -j4)
