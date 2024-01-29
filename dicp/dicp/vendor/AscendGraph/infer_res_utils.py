a16c4b2c45a439ac5d2c26197a46bcc5aeedaa1c
Reinerzhou <87467364+Reinerzhou@users.noreply.github.com>
2024-01-29 14:30:52 +0800

    [DICP][tops] Implement paras auto cast in some binary ops. (#615)
    * implement paras auto cast in some binary ops.
    
    * unify dtype cast code.

--- dicp/dicp/vendor/AscendGraph/infer_res_utils.py
+++ dicp/dicp/vendor/AscendGraph/infer_res_utils.py
@@ -1,6 +1,6 @@
 from collections.abc import Sequence
 from typing import Optional, Tuple, Union, List
-from dicp.dynamo_bridge.utils import get_memory_format
+from dicp.dynamo_bridge.utils import get_memory_format, get_cast_dtype
 
 import torch
 import math
@@ -76,50 +76,6 @@ def get_op_const_arg_kwarg(
 
 
 """analyze dtype,format"""
-
-
-def get_cast_dtype(
-    type1: Union[str, torch.dtype, type], type2: Union[str, torch.dtype, type]
-) -> Union[str, torch.dtype, None]:
-    type_map = {
-        int: torch.int,
-        float: torch.float,
-        complex: torch.complex,
-        bool: torch.bool,
-    }
-
-    type1 = torch.dtype(type1) if isinstance(type1, str) else type1
-    type2 = torch.dtype(type2) if isinstance(type2, str) else type2
-
-    type1 = type_map[type1] if isinstance(type1, type) else type1
-    type2 = type_map[type2] if isinstance(type2, type) else type2
-
-    if type1 == type2:
-        return type1
-
-    complex_list = [torch.complex32, torch.complex64, torch.complex128]
-    float_list = [torch.float16, torch.float32, torch.float, torch.float64]
-    int_list = [torch.int8, torch.int16, torch.int32, torch.int, torch.int64]
-
-    if type1 in complex_list or type2 in complex_list:
-        t1_idx = complex_list.index(type1) if type1 in complex_list else -1
-        t2_idx = complex_list.index(type2) if type2 in complex_list else -1
-        return complex_list[max(t1_idx, t2_idx)]
-
-    elif type1 == torch.double or type2 == torch.double:
-        return torch.double
-    elif type1 in float_list or type2 in float_list:
-        t1_idx = float_list.index(type1) if type1 in float_list else -1
-        t2_idx = float_list.index(type2) if type2 in float_list else -1
-        return float_list[max(t1_idx, t2_idx)]
-    elif type1 in int_list or type2 in int_list:
-        t1_idx = int_list.index(type1) if type1 in int_list else -1
-        t2_idx = int_list.index(type2) if type2 in int_list else -1
-        return int_list[max(t1_idx, t2_idx)]
-    elif type1 == torch.bool or type2 == torch.bool:
-        return torch.bool
-
-    assert False, str(type1) + " " + str(type2) + " can't cast these two types!"
 
 
 def analyze_memory_format(tensor: torch.Tensor, operation: str) -> torch.memory_format:
