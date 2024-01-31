a0b6022e8188636892fb499565f3aba5870f2e50
jinminxi104 <jinminxi104@hotmail.com>
2024-02-01 00:51:20 +0800

    Revert "[DIPU] Check SupportedDiopiFunctions.txt and upgrade to clang17 (#633)"
    This reverts commit c61f4cf8eae12227c1286c29a8e973fa01936292.

--- dipu/scripts/ci/nv/ci_nv_tidy.sh
+++ dipu/scripts/ci/nv/ci_nv_tidy.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -euo pipefail
-
-# Try finding clangd and libstdc++.so.6 on SCO.
-# Note 1: ":+:" is used to handle unbound variable.
-[ -f /mnt/cache/share/deeplinkci/github/proxy_on ] &&
-    source /mnt/cache/share/deeplinkci/github/proxy_on
-[ -d /mnt/cache/share/platform/dep/clang-17/bin ] &&
-    export PATH=/mnt/cache/share/platform/dep/clang-17/bin${PATH:+:$PATH}
-
-# Required tools.
-[ -x "$(command -v git)" ] || { echo "::error::Missing git tool" && exit 1; }
-[ -x "$(command -v clangd)" ] || { echo "::error::Missing clangd tool" && exit 1; }
+source /mnt/cache/share/deeplinkci/github/proxy_on
+# Require Git.
+[ -x "$(command -v git)" ] || (echo "missing git tool" && exit 1)
 
 # Get current folder.
-self=$(dirname "$(realpath -s "${BASH_SOURCE[0]}")")
-repo=$(cd "$self" && git rev-parse --show-toplevel)
+self=$(dirname $(realpath -s $0))
+repo=$(cd $self && git rev-parse --show-toplevel)
 
 # Download clangd-tidy scripts.
 [ -d "$self/clangd-tidy" ] ||
-    git -c advice.detachedHead=false clone --depth 1 -b v0.1.2 https://github.com/lljbash/clangd-tidy.git "$self/clangd-tidy"
+    git -c advice.detachedHead=false clone --depth 1 -b v0.1.0 https://github.com/lljbash/clangd-tidy.git "$self/clangd-tidy"
 
-# Collect source files and run tidy.
+# Try finding clangd and libstdc++.so.6 on 1988.
+# Note: ":+:" is used to handle unbound variable.
+[ -d /mnt/cache/share/platform/dep/clang-16/bin ] &&
+    export PATH=/mnt/cache/share/platform/dep/clang-16/bin${PATH:+:$PATH}
+[ -d /mnt/cache/share/platform/env/miniconda3.10/envs/pt2.0_diopi/lib ] &&
+    export LD_LIBRARY_PATH=/mnt/cache/share/platform/env/miniconda3.10/envs/pt2.0_diopi/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+
+# Forward srun commands.
+# e.g. you can use: bash scripts/ci/nv/ci_nv_tidy.sh srun -p pat_rd
 (cd "$repo/dipu" &&
-    find torch_dipu ! -path '*/vendor/*' ! -name 'AutoGenedKernels.cpp' \( -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) |
-    xargs "$self/clangd-tidy/clangd-tidy" -j4)
+    (find torch_dipu ! -path '*/vendor/*' ! -name AutoGenedKernels.cpp \( -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) |
+        xargs $self/clangd-tidy/clangd-tidy -j4))
