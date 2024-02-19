9cfa8fe3c59bebea33b7e7bc0accc037d5571c2e
jfxu-st <xujinfan@sensetime.com>
2024-02-19 20:20:12 +0800

    add custom fallbacks for sigmoid.out, sigmoid_backward.grad_input

--- dipu/torch_dipu/csrc_dipu/aten/ops/CustomFallbackFunctions.hpp
+++ dipu/torch_dipu/csrc_dipu/aten/ops/CustomFallbackFunctions.hpp
@@ -462,5 +462,23 @@ static at::Tensor& custom_fallback_dipu__softmax_out(const at::Tensor& self,
   return out;
 }
 
+static at::Tensor& custom_fallback_dipu_sigmoid_out(const at::Tensor& self,
+                                                    at::Tensor& out) {
+  auto self_cpu = to_cpu_with_half_to_float(self);
+  auto out_cpu = at::sigmoid(self_cpu);
+  out.copy_(out_cpu);
+  return out;
+}
+
+static at::Tensor& custom_fallback_dipu_sigmoid_backward_grad_input(
+    const at::Tensor& grad_output, const at::Tensor& output,
+    at::Tensor& grad_input) {
+  auto grad_output_cpu = to_cpu_with_half_to_float(grad_output);
+  auto output_cpu = to_cpu_with_half_to_float(output);
+  auto grad_input_cpu = at::sigmoid_backward(grad_output_cpu, output_cpu);
+  grad_input.copy_(grad_input_cpu);
+  return grad_input;
+}
+
 }  // namespace native
 }  // namespace dipu
