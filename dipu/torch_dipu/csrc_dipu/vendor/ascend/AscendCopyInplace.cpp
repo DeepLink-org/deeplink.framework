0e5fb14a4991c41bdc8bda7d7c57f08a1aceba49
jfxu-st <143591296+jfxu-st@users.noreply.github.com>
2024-02-27 11:12:00 +0800

    [DIPU] Remove unnecessary sync in copyPostProcess for Ascend (#702)
    * remove unnecessary sync in copyPostProcess for Ascend
    
    * add TODO

---
+++ dipu/torch_dipu/csrc_dipu/vendor/ascend/AscendCopyInplace.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2023, DeepLink.
+
+#include <csrc_dipu/aten/ops/DIPUCopy.hpp>
+#include <csrc_dipu/runtime/core/DIPUStream.h>
+
+namespace dipu {
+
+class AscendCopyInplace : public DIPUCopyInpOnDIOPI {
+ public:
+  AscendCopyInplace() = default;
+  ~AscendCopyInplace() override = default;
+
+ protected:
+  void copyPostProcess(bool non_blocking, const CopyParamsInfo& info,
+                       DIPUStream& curStream) override {
+    // TODO(fandaoyi): Refactor to remove duplicated code from different vendors
+    // Ref: https://pytorch.org/docs/stable/generated/torch.Tensor.copy_.html
+    // In d2self cases, non_blocking has no effect.
+    // For other cases, do sync after copy if non_blocking is false.
+    if (!non_blocking && info.copyType_ != DIPUCopyType::D2Self) {
+      dipu::devapis::syncStream(curStream.rawstream());
+    }
+  }
+};
+
+// not const, see comments in DIPUCopy.cpp dipu_copy_op()
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+static AscendCopyInplace ascend_copy_inplace;
+
+// this variable only for call setInst. no other use
+const static int32_t ascend_init = []() {
+  setDipuCopyInstance(&ascend_copy_inplace);
+  return 1;
+}();
+
+}  // namespace dipu
