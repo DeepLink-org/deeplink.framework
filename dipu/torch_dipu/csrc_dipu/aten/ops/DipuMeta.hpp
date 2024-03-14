e6d3e277d0f24e80c33d7abb784b7c79c815464a
root <yangbofun@163.com>
2024-03-14 19:04:40 +0800

    version 2

--- dipu/torch_dipu/csrc_dipu/aten/ops/DipuMeta.hpp
+++ dipu/torch_dipu/csrc_dipu/aten/ops/DipuMeta.hpp
@@ -1,77 +1,101 @@
+#include <cstdint>
 #include <ATen/NamedTensorUtils.h>
 #include <ATen/TensorMeta.h>
 #include <ATen/native/BinaryOps.h>
 #include <ATen/ops/add_meta.h>
+#include <c10/core/ScalarType.h>
+#include <c10/util/ArrayRef.h>
+#include <c10/util/SmallVector.h>
+#include <ATen/native/TypeProperties.h>
+#include <ATen/ExpandUtils.h>
+#include <ATen/native/BinaryOps.h>
 
+
+#include <iostream>
 #include "csrc_dipu/aten/DIPUATenFunctions.h"
+#include "csrc_dipu/aten/ops/DIPUAmp.hpp"
 
+class InferConfig {};
 #define DIPU_BINARY_OP_CONFIG()             \
-  at::TensorIteratorConfig()                \
-      .set_check_mem_overlap(true)          \
-      .allow_cpu_scalars(true)              \
-      .promote_inputs_to_common_dtype(true) \
-      .cast_common_dtype_to_outputs(false)  \
-      .enforce_safe_casting_to_output(false)
+  InferConfig()
 
 #define DIPU_TORCH_META_FUNC2(name, overload) \
   void Infer_##name##_##overload::meta
 
-at::Tensor create_out(at::IntArrayRef sizes, at::IntArrayRef strides,
-                      const c10::TensorOptions& options) {
-  if (strides.empty()) {
-    return dipu::native::dipu_aten::empty(
-        sizes, options.dtype_opt()->toScalarType(), options.layout_opt(),
-        options.device_opt(), options.pinned_memory_opt(),
-        options.memory_format_opt());
-  } else {
-    return dipu::native::dipu_aten::empty_strided(
-        sizes, strides, options.dtype_opt()->toScalarType(),
-        options.layout_opt(), options.device_opt(),
-        options.pinned_memory_opt());
+
+class Infer {
+  public:
+    Infer& add_input(const at::Tensor* p_tensor) {
+      p_tensors_.push_back(p_tensor);
+      return *this;
+    }
+
+    Infer& set_config(const InferConfig& config){
+      config_ = config;
+      return *this;
+    }
+
+    void build(){
+      compute_common_dtype();
+      compute_shape();
+    }
+    c10::IntArrayRef target_shape() { return target_shape_; }
+    at::ScalarType common_dtype() { return common_dtype_; }
+
+   private:
+    void compute_common_dtype();
+    void compute_shape();
+
+    c10::SmallVector<const at::Tensor*, 4> p_tensors_={};
+    c10::IntArrayRef target_shape_={};
+    at::ScalarType common_dtype_ = at::ScalarType::Undefined;
+    InferConfig config_{};
+};
+
+void Infer::compute_common_dtype() {
+  at::native::ResultTypeState state = {};
+  for (const auto p_tensor : p_tensors_) {
+    state = at::native::update_result_type_state(*p_tensor, state);
+  }
+
+  common_dtype_ = at::native::result_type(state);
+  TORCH_INTERNAL_ASSERT(common_dtype_ != at::ScalarType::Undefined);
+}
+
+void Infer::compute_shape() {
+  bool all_ops_same_shape = true;
+  bool has_scalars = false;
+  bool has_tensors = false;
+  for (const auto p_tensor : p_tensors_) {
+    if (!p_tensor->defined()) {
+      continue;
+    }
+
+    auto shape = p_tensor->sizes();
+    if (shape.empty()) {
+      has_scalars = true;
+    } else {
+      has_tensors = true;
+    }
+    if (has_scalars && has_tensors) {
+      all_ops_same_shape = false;
+    }
+    if (target_shape_.empty()) {
+      target_shape_ = shape;
+    } else if (!shape.equals(target_shape_)) {
+      all_ops_same_shape = false;
+      target_shape_ = at::infer_size_dimvector(target_shape_, shape);
+    }
   }
 }
 
-struct Infer_add_Tensor final : public at::meta::structured_add_Tensor {
-  void meta(const at::Tensor& self, const at::Tensor& other,
-            const at::Scalar& alpha);
 
-  void set_output_strided(int64_t output_idx, at::IntArrayRef sizes,
-                          at::IntArrayRef strides, c10::TensorOptions options,
-                          at::DimnameList names) override {
-    outputs_[output_idx] = create_out(sizes, strides, options);
-    if (!names.empty()) {
-      at::namedinference::propagate_names(outputs_[output_idx], names);
-    }
-    // super must happen after, so that downstream can use maybe_get_output
-    // to retrieve the output
-    at::meta::structured_add_Tensor::set_output_raw_strided(
-        output_idx, sizes, strides, options, names);
-  }
-  void set_output_raw_strided(int64_t output_idx, at::IntArrayRef sizes,
-                              at::IntArrayRef strides,
-                              c10::TensorOptions options,
-                              at::DimnameList names) override {
-    outputs_[output_idx] = create_out(sizes, strides, options);
-    if (!names.empty()) {
-      at::namedinference::propagate_names(outputs_[output_idx], names);
-    }
-    // super must happen after, so that downstream can use maybe_get_output
-    // to retrieve the output
-    at::meta::structured_add_Tensor::set_output_raw_strided(
-        output_idx, sizes, strides, options, names);
-  }
-  const at::Tensor& maybe_get_output(int64_t output_idx) override {
-    return outputs_[output_idx];
-  }
-  const at::Tensor& maybe_get_output() { return maybe_get_output(0); }
-  std::array<at::Tensor, 1> outputs_;
+struct Infer_add_Tensor final : public Infer {
+    void meta(const at::Tensor& self, const at::Tensor& other,
+              const at::Scalar& alpha);
 };
 
 DIPU_TORCH_META_FUNC2(add, Tensor)
 (const at::Tensor& self, const at::Tensor& other, const at::Scalar& alpha) {
-  build(DIPU_BINARY_OP_CONFIG()
-            .add_output(maybe_get_output())
-            .add_input(self)
-            .add_input(other));
-  at::native::alpha_check(dtype(), alpha);
+  set_config(DIPU_BINARY_OP_CONFIG()).add_input(&self).add_input(&other).build();
 }
