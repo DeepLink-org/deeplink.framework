aa29986d90db3ea6697a9f6424362c42d91071fb
zhangzefeng92 <zhang_zefeng@foxmail.com>
2024-03-22 11:53:06 +0800

    add unittest for equal op

--- dipu/tests/python/unittests/test_equal.py
+++ dipu/tests/python/unittests/test_equal.py
@@ -9,26 +9,26 @@ class TestEqual(TestCase):
     @onlyOn("NPU")
     def test_equal_1(self):
         device = torch.device("dipu")
-        x = torch.rand(3,4).to(device)
+        x = torch.rand(3, 4).to(device)
         self.assertEqual(True, torch.equal(x, x))
 
     @onlyOn("NPU")
     def test_equal_2(self):
         device = torch.device("dipu")
-        x = torch.rand(3,4).to(device)
+        x = torch.rand(3, 4).to(device)
         self.assertEqual(False, torch.equal(x, x.to(torch.float16)))
 
     @onlyOn("NPU")
     def test_equal_3(self):
         device = torch.device("dipu")
-        x = torch.zeros(3,4).to(device)
+        x = torch.zeros(3, 4).to(device)
         self.assertEqual(True, torch.equal(x, x.to(torch.float16)))
 
     @onlyOn("NPU")
     def test_equal_4(self):
         device = torch.device("dipu")
-        x = torch.rand(3,4).to(device)
-        y = torch.rand(3,5).to(device)
+        x = torch.rand(3, 4).to(device)
+        y = torch.rand(3, 5).to(device)
         self.assertEqual(False, torch.equal(x, y))
 
 
