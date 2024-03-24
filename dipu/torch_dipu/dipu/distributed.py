1f13d1927676b6d5d18ba34d20690f052918be40
fandaoyi <fandaoyi@sensetime.com>
2024-03-24 10:32:40 +0000

    add batch wrap

--- dipu/torch_dipu/dipu/distributed.py
+++ dipu/torch_dipu/dipu/distributed.py
@@ -39,6 +39,7 @@ _raw_register_backend = ProcessGroup._register_backend
 def _wrapped_register_backend(self, device, backend_type, backend):
     # dicl not support cpu tensor
     if device.type == "cpu" and isinstance(backend, ProcessGroupDICL):
+        print(" try create a gloo backend,",  backend.size(), flush=True)
         backend = ProcessGroupGloo(
             backend.store(), backend.rank(), backend.size(), timeout=backend.timeout()
         )
@@ -85,8 +86,31 @@ def _wrap_get_backend(group: Optional[ProcessGroup] = None) -> str:
     else:
         return ret
 
+# dicl not support coalescing now. Todo: remove after support
+def wrap_batch_isend_irecv(p2p_op_list):
+    dist.distributed_c10d._check_p2p_op_list(p2p_op_list)
+    reqs = []
+    for p2p_op in p2p_op_list:
+        work = p2p_op.op(p2p_op.tensor, p2p_op.peer, p2p_op.group, p2p_op.tag)
+        if work:
+            reqs.append(work)
+    return reqs
+
+# huawei AscendSpeed pass rank list like [0, 0], which cause pg 
+# creation fail in torch 2.0. actually it's huawei's problem, such input
+# is not valid, but nothing else we can do.
+# torch 2.1 handle duplication rank inner
+_raw_new_group = dist.new_group
+def wrap_new_group(ranks=None, timeout=default_pg_timeout, backend=None, pg_options=None):
+    ranks = list(set(ranks))  # dedup
+    return _raw_new_group(ranks, timeout, backend, pg_options)
+
 
 def apply_dist_patch():
     dist.get_backend = _wrap_get_backend
     dist.init_process_group = _wrap_init_process_groups
     dist.ProcessGroup._register_backend = _wrapped_register_backend
+    dist.batch_isend_irecv = wrap_batch_isend_irecv
+
+    if dipu.get_dipu_torch_version() == dipu.torch_ver_200:
+        dist.new_group = wrap_new_group
