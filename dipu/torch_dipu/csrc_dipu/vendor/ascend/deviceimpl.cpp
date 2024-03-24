102b1c47f05485d27dbfda638199057ed090a911
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-03-24 07:03:12 +0000

    optimize setDevice performance

--- dipu/torch_dipu/csrc_dipu/vendor/ascend/deviceimpl.cpp
+++ dipu/torch_dipu/csrc_dipu/vendor/ascend/deviceimpl.cpp
@@ -19,10 +19,7 @@ namespace devapis {
 using ascend_deviceId = int32_t;
 thread_local int currentDeviceIndex = -1;
 
-void initializeVendor() {
-  DIPU_CALLACLRT(aclInit(nullptr));
-  DIPU_CALLACLRT(aclrtSetDeviceSatMode(ACL_RT_OVERFLOW_MODE_INFNAN));
-}
+void initializeVendor() { DIPU_CALLACLRT(aclInit(nullptr)); }
 
 void finalizeVendor() { DIPU_CALLACLRT(aclFinalize()); }
 
@@ -46,9 +43,14 @@ void setDefalutDevice(int index) {
 void setDevice(deviceId_t devId) {
   // In order to reduce performance loss, try to reduce the number of reads and
   // writes of atomic variables.
+  // Atomic variables will only be manipulated when starting up.
+  // In most other cases, reading and writing atomic variables is no longer required.
+  // This function is called extremely frequently.
   if (devId < 0) {
     if (defaultDeviceIndex < 0) {
-      setDefalutDevice(0);
+      if (defalutDeviceIndexAtomic < 0) {
+        setDefalutDevice(0);
+      }
     }
     devId = defalutDeviceIndexAtomic;
   } else {
@@ -62,12 +64,11 @@ void setDevice(deviceId_t devId) {
     TORCH_WARN_ONCE(
         "Trying to use multiple cards in the same process may cause unexpected "
         "results in hccl communication, such as sdma memory copy failure");
-  } else {
-    ascend_deviceId devId_ = static_cast<deviceId_t>(devId);
-    if (devId_ != currentDeviceIndex) {
-      DIPU_CALLACLRT(::aclrtSetDevice(devId_))
-      currentDeviceIndex = devId_;
-    }
+  }
+  ascend_deviceId devId_ = static_cast<deviceId_t>(devId);
+  if (devId_ != currentDeviceIndex) {
+    DIPU_CALLACLRT(::aclrtSetDevice(devId_))
+    currentDeviceIndex = devId_;
   }
 }
 
@@ -134,10 +135,7 @@ OpStatus mallocDevice(void** p, size_t nbytes, bool throwExcepion) {
     *p = nullptr;
     return OpStatus::SUCCESS;
   }
-  // 若用户需申请大块内存并自行划分、管理内存时，建议使用aclrtMallocAlign32接
-  // 口，该接口相比aclrtMalloc接口，只会对用户申请的size向上对齐成32字节整数
-  // 倍，不会再多加32字节。 大块内存用作缓存时，无需多加32字节
-  DIPU_CALLACLRT(::aclrtMallocAlign32(p, nbytes, ACL_MEM_MALLOC_HUGE_FIRST));
+  DIPU_CALLACLRT(::aclrtMalloc(p, nbytes, ACL_MEM_MALLOC_HUGE_FIRST));
   return OpStatus::SUCCESS;
 }
 
@@ -151,18 +149,21 @@ void freeDevice(void* p) {
 // (synchronous) copy from device to a device
 void memCopyD2D(size_t nbytes, deviceId_t dstDevId, void* dst,
                 deviceId_t srcDevId, const void* src) {
+  syncDevice();
   DIPU_CALLACLRT(
       ::aclrtMemcpy(dst, nbytes, src, nbytes, ACL_MEMCPY_DEVICE_TO_DEVICE));
 }
 
 // (synchronous) copy from host to a device
 void memCopyH2D(size_t nbytes, void* dst, const void* src) {
+  syncDevice();
   DIPU_CALLACLRT(
       ::aclrtMemcpy(dst, nbytes, src, nbytes, ACL_MEMCPY_HOST_TO_DEVICE));
 }
 
 // (synchronous) copy from a device to host
 void memCopyD2H(size_t nbytes, void* dst, const void* src) {
+  syncDevice();
   DIPU_CALLACLRT(
       ::aclrtMemcpy(dst, nbytes, src, nbytes, ACL_MEMCPY_DEVICE_TO_HOST));
 }
@@ -250,7 +251,7 @@ bool streamNotNull(deviceStream_t stream) {
 }
 
 void streamWaitEvent(deviceStream_t stream, deviceEvent_t event) {
-  DIPU_CALLACLRT(::aclrtStreamWaitEvent(stream, event))
+  DIPU_CALLACLRT(::aclrtSynchronizeEvent(event))
 }
 
 // =====================
