4bbe4fbac63ad45bce46e6de6fe1c60ede369591
Lingjie <lljbash@gmail.com>
2024-03-29 18:12:41 +0800

    test(dipu,python): enhance test_python_device.py

--- dipu/tests/python/unittests/test_python_device.py
+++ dipu/tests/python/unittests/test_python_device.py
@@ -4,7 +4,7 @@ import torch_dipu
 from torch_dipu.testing._internal.common_utils import TestCase, run_tests
 
 
-class TestAbs(TestCase):
+class TestPythonDevice(TestCase):
     def test_cpu(self):
         a = torch.tensor([1, 2, 3])
         self.assertEqual(str(a.device), "cpu")
@@ -13,12 +13,13 @@ class TestAbs(TestCase):
         self.assertEqual(repr(a), "tensor([1, 2, 3])")
 
     def test_cuda(self):
-        torch.cuda.set_device(0)
+        device_index = 0  # NOTE: maybe 0 is not available, fix me if this happens
+        torch.cuda.set_device(device_index)
         a = torch.tensor([1, 2, 3]).cuda()
-        self.assertEqual(str(a.device), "cuda:0")
-        self.assertEqual(repr(a.device), "device(type='cuda', index=0)")
-        self.assertEqual(str(a), "tensor([1, 2, 3], device='cuda:0')")
-        self.assertEqual(repr(a), "tensor([1, 2, 3], device='cuda:0')")
+        self.assertEqual(str(a.device), f"cuda:{device_index}")
+        self.assertEqual(repr(a.device), f"device(type='cuda', index={device_index})")
+        self.assertEqual(str(a), f"tensor([1, 2, 3], device='cuda:{device_index}')")
+        self.assertEqual(repr(a), f"tensor([1, 2, 3], device='cuda:{device_index}')")
 
 
 if __name__ == "__main__":
