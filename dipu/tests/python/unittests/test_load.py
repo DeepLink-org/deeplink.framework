2d6f623ff25d74763a5c53f82e278c13144f3d04
lwysense <147478868+lwysense@users.noreply.github.com>
2024-04-08 17:06:32 +0800

    fix(dipu): Bug in loading tensor (#768)
    * fix: Bug in loading tensor
    
    * fix format
    
    * change format
    
    * add unit test
    
    * fix format

---
+++ dipu/tests/python/unittests/test_load.py
@@ -0,0 +1,21 @@
+import tempfile
+import os
+import torch
+import torch_dipu
+from torch_dipu.testing._internal.common_utils import TestCase, run_tests
+
+
+class TestLoad(TestCase):
+    def test_load(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            save_p = os.path.join(tmpdir, "test_load.pth")
+
+            t = torch.randn(3, 3).cuda()
+            torch.save(t, save_p)
+            t_load = torch.load(save_p)
+
+            self.assertEqual(t, t_load, prec=0)
+
+
+if __name__ == "__main__":
+    run_tests()
