ddebc55c7f61054fbbd2a7097e39adc7abc53e87
lwysense <liuweiyu@sensetime.com>
2024-04-08 16:24:24 +0800

    fix format

--- dipu/tests/python/unittests/test_load.py
+++ dipu/tests/python/unittests/test_load.py
@@ -9,13 +9,13 @@ class TestLoad(TestCase):
     def test_load(self):
         with tempfile.TemporaryDirectory() as tmpdir:
             save_p = os.path.join(tmpdir, "test_load.pth")
-            
+
             t = torch.randn(3, 3).cuda()
-            torch.save(t, save_p) 
+            torch.save(t, save_p)
             t_load = torch.load(save_p)
 
             self.assertEqual(t, t_load, prec=0)
 
 
 if __name__ == "__main__":
-    run_tests() 
+    run_tests()
