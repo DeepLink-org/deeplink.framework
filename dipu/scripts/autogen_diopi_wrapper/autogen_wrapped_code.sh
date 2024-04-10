c7616ec0ebee3220c5625b035ddcc19b5d8b520a
root <yangbofun@163.com>
2024-04-10 03:07:53 +0000

    fix

--- dipu/scripts/autogen_diopi_wrapper/autogen_wrapped_code.sh
+++ dipu/scripts/autogen_diopi_wrapper/autogen_wrapped_code.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-DIPU_DIR=$(dirname $(dirname $(dirname "$0")))
-USE_AUTOCOMPARE="$1"
-UsedVendor="$2"
-Torch_VERSION="$3"
+DIPU_DIR=$(readlink -f $(dirname $(readlink -f "$0"))/../..)
+AUTOGEN_DIOPI_WRAPPER=$DIPU_DIR/scripts/autogen_diopi_wrapper
 
-# The following parameters are used to auto_generated code.
-# These parameters are also used in ../../torch_dipu/csrc_dipu/CMakeLists.txt
-# Please do not modify these parameters separately.
-GENERATED_KERNELS=${DIPU_DIR}/torch_dipu/csrc_dipu/aten/ops/AutoGenedKernels.cpp
-GENERATED_KERNELS_SCRIPT=${DIPU_DIR}/scripts/autogen_diopi_wrapper/autogen_diopi_wrapper.py
-GENERATED_KERNELS_CONFIG=${DIPU_DIR}/scripts/autogen_diopi_wrapper/diopi_functions.yaml
+USE_AUTOCOMPARE=${1:-OFF}
+UsedVendor=${2:-cuda}
+Torch_VERSION=${3:-2.1.0}
+GENERATED_KERNELS_SCRIPT=${4:-$AUTOGEN_DIOPI_WRAPPER/autogen_diopi_wrapper.py}
+GENERATED_KERNELS_CONFIG=${5:-$AUTOGEN_DIOPI_WRAPPER/diopi_functions.yaml}
+GENERATED_KERNELS=${6:-$DIPU_DIR/torch_dipu/csrc_dipu/aten/ops/AutoGenedKernels.cpp}
+
 GENERATED_KERNELS_VENDOR=${DIPU_DIR}/third_party/DIOPI/impl/${UsedVendor}/convert_config.yaml
 
 PYTHON_CMD="python3 ${GENERATED_KERNELS_SCRIPT} --out=${GENERATED_KERNELS} --config=${GENERATED_KERNELS_CONFIG} \
