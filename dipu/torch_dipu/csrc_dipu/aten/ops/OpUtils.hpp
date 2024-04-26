41dfa344e06f7cf38f1e22d62af241aba792ede6
yewentao256 <zhyanwentao@126.com>
2024-04-26 14:33:32 +0800

    rename is_wrapped_number to is_scalar_on_cpu

--- dipu/torch_dipu/csrc_dipu/aten/ops/OpUtils.hpp
+++ dipu/torch_dipu/csrc_dipu/aten/ops/OpUtils.hpp
@@ -251,7 +251,7 @@ at::ScalarType mixed_output_scalar_type(const at::Tensor& input,
   return at::native::param_scalar_type(input, mixed);
 }
 
-inline bool is_wrapped_number(const at::Tensor& t) {
+inline bool is_scalar_on_cpu(const at::Tensor& t) {
   return t.unsafeGetTensorImpl()->is_wrapped_number();
 }
 
