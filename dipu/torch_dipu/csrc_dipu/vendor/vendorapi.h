49ecc5e13fec89b17e863d505598c44b944d1502
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-04-26 15:42:26 +0800

    fix no need change

---
+++ dipu/torch_dipu/csrc_dipu/vendor/vendorapi.h
@@ -0,0 +1,43 @@
+// Copyright (c) 2023, DeepLink.
+#pragma once
+
+#include <acl/acl.h>
+#include <acl/acl_op.h>
+#include <acl/acl_op_compiler.h>
+
+#include <hccl/hccl.h>
+#include <hccl/hccl_types.h>
+
+#include <c10/util/Exception.h>
+
+#include <csrc_dipu/common.h>
+
+// #include <csrc_dipu/vendor/native_memory_format.hpp>  // IWYU pragma: export
+
+namespace dipu {
+
+using deviceStream_t = aclrtStream;
+#define deviceDefaultStreamLiteral nullptr;
+using deviceEvent_t = aclrtEvent;
+using deviceHandle_t = aclrtContext*;
+
+using diclComm_t = HcclComm;
+using commUniqueId = HcclRootInfo;
+
+enum class NativeMemoryFormat_t : int64_t {
+  UNDEFINED = -1,
+  NCHW = 0,
+  NHWC = 1,
+  ND = 2,
+  NC1HWC0 = 3,
+  FRACTAL_Z = 4,
+  NC1HWC0_C04 = 12,
+  HWCN = 16,
+  NDHWC = 27,
+  FRACTAL_NZ = 29,
+  NCDHW = 30,
+  NDC1HWC0 = 32,
+  FRACTAL_Z_3D = 33
+};
+
+}  // namespace dipu
