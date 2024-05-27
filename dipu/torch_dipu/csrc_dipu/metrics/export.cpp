cfda7e5d0bee8714d17787ddccf97bfa855a8ac9
wiryls <7984500+wiryls@users.noreply.github.com>
2024-05-27 23:06:06 +0800

    debug: avoid destruction

--- dipu/torch_dipu/csrc_dipu/metrics/export.cpp
+++ dipu/torch_dipu/csrc_dipu/metrics/export.cpp
@@ -8,18 +8,18 @@
 namespace dipu {
 
 auto default_metrics_collector() -> metrics::collector<char>& {
-  auto static instance = std::make_unique<metrics::collector<char>>();
+  auto static instance = new metrics::collector<char>();
   return *instance;
 }
 
 auto default_device_allocator_metrics_producer() -> allocator_metrics& {
-  auto static instance = std::make_unique<allocator_metrics>(
+  auto static instance = new allocator_metrics(
       default_metrics_collector(), metrics::labelset<char>{{"type", "device"}});
   return *instance;
 }
 
 auto default_host_allocator_metrics_producer() -> allocator_metrics& {
-  auto static instance = std::make_unique<allocator_metrics>(
+  auto static instance = new allocator_metrics(
       default_metrics_collector(), metrics::labelset<char>{{"type", "host"}});
   return *instance;
 }
