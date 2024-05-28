88ff12325d6ab34dc5047a3ef24c5d0a3673e8d6
jfxu-st <xujinfan@sensetime.com>
2024-05-28 10:39:50 +0800

    update kDefaultMaxAsyncResourcePoolLength to adapt to the new faster implementation of async resource pool

--- dipu/torch_dipu/csrc_dipu/runtime/core/allocator/DIPUCachingAllocator.cpp
+++ dipu/torch_dipu/csrc_dipu/runtime/core/allocator/DIPUCachingAllocator.cpp
@@ -20,7 +20,7 @@ namespace dipu {
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 std::mutex DIPURawDeviceAllocator::mutex_;
 
-constexpr size_t kDefaultMaxAsyncResourcePoolLength = 64;
+constexpr size_t kDefaultMaxAsyncResourcePoolLength = 96;
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 const size_t kMaxAsyncResourcePoolLength = get_env_or_default(
     "DIPU_MAX_ASYNC_RESOURCE_POOL_LENGTH", kDefaultMaxAsyncResourcePoolLength);
