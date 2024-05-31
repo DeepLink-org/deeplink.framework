db865d1674c2509b33a03c18130c2dae35d75d3a
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-05-31 07:27:27 +0000

    add env to control align size on ascend

--- dipu/torch_dipu/csrc_dipu/vendor/ascend/AscendCustomAllocatorStrategy.cpp
+++ dipu/torch_dipu/csrc_dipu/vendor/ascend/AscendCustomAllocatorStrategy.cpp
@@ -1,10 +1,14 @@
 #include "csrc_dipu/runtime/core/allocator/DIPUCachingAllocator.h"
+#include "csrc_dipu/utils/env.hpp"
+#include <cstdint>
 
 const static int ascend_init = []() {
   // – 内存大小向上对齐成32整数倍+32字节(m=ALIGN_UP[len,32]+32字节);
   // – 内存起始地址需满足64字节对齐(ALIGN_UP[m,64])。
   //  nbytes = align_64(1 * nbytes + 32);
-  static dipu::MemoryAlignmentStrategy memoryAlignStrategy(64, 1, 32);
+
+  int64_t align = dipu::get_env_or_default("ASCEND_ALLOCATE_ALIGN", 64);
+  static dipu::MemoryAlignmentStrategy memoryAlignStrategy(align, 1, 32);
   dipu::setMemoryAlignmentStrategy(&memoryAlignStrategy);
   return 0;
 }();
