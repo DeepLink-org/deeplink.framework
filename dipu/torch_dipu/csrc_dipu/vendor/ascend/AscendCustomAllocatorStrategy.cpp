74ba1bc7fdf4d096ec89bcee98c3634dcf0f0b63
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-06-20 07:03:22 +0000

    Use 512-byte alignment on Ascend

--- dipu/torch_dipu/csrc_dipu/vendor/ascend/AscendCustomAllocatorStrategy.cpp
+++ dipu/torch_dipu/csrc_dipu/vendor/ascend/AscendCustomAllocatorStrategy.cpp
@@ -7,7 +7,7 @@ const static int ascend_init = []() {
   // – 内存起始地址需满足64字节对齐(ALIGN_UP[m,64])。
   //  nbytes = align_64(1 * nbytes + 32);
 
-  int64_t align = dipu::get_env_or_default("ASCEND_ALLOCATE_ALIGN", 64);
+  int64_t align = dipu::get_env_or_default("ASCEND_ALLOCATE_ALIGN", 512);
   static dipu::MemoryAlignmentStrategy memoryAlignStrategy(align, 1, 32);
   dipu::setMemoryAlignmentStrategy(&memoryAlignStrategy);
   return 0;
