6894694900e49bef65fb78a1e0c0c4d712303450
wiryls <7984500+wiryls@users.noreply.github.com>
2024-07-05 15:14:11 +0800

    refactor: better setup.py

--- dipu/setup.py
+++ dipu/setup.py
@@ -1,18 +1,7 @@
-# Copyright (c) 2023, DeepLink.
-import glob
-import multiprocessing
-import multiprocessing.pool
 import os
-import re
-import shutil
-import subprocess
 import sys
-import platform
 import setuptools
 from skbuild import setup
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-VERSION = "0.1"
 
 try:
     import torch
@@ -23,82 +12,74 @@ import torch
 import builtins
 
 
-def get_DIPU_ABI_V():
-    return next(
-        item[-4:-2]
-        for item in dir(builtins)
-        if "__pybind11_internals_v4_gcc_libstdcpp_cxxabi10" in item
-    )
-
-
-def get_DIPU_COMPILED_WITH_CXX11_ABI():
-    return 1 if torch.compiled_with_cxx11_abi() else 0
-
-
-def get_DIOPI_CMAKE_PREFIX_PATH():
-    return torch.utils.cmake_prefix_path
-
-
-def get_PYTORCH_DIR():
-    return os.path.dirname(torch.__path__[0])
-
-
 def customized_cmake_args():
-    cmake_args = list()
+    # TODO(refactor): better error checking
+
+    def dipu_abi_v():
+        return next(
+            item[-4:-2]
+            for item in dir(builtins)
+            if "__pybind11_internals_v4_gcc_libstdcpp_cxxabi10" in item
+        )
+
+    def dipu_compiled_with_cxx11_abi():
+        return 1 if torch.compiled_with_cxx11_abi() else 0
+
+    def diopi_cmake_prefix_path():
+        return torch.utils.cmake_prefix_path
+
+    def pytorch_dir():
+        return os.path.dirname(torch.__path__[0])
 
     cmake_with_diopi_library = os.getenv("DIPU_WITH_DIOPI_LIBRARY", "INTERNAL")
     cmake_device = os.getenv("DIPU_DEVICE", "cuda")
     cmake_use_coverage = os.getenv("USE_COVERAGE", "OFF")
-    cmake_args.append("-DCMAKE_BUILD_TYPE=Release")
-    cmake_args.append("-DDEVICE=" + cmake_device)
-    cmake_args.append("-DENABLE_COVERAGE=${USE_COVERAGE}")
-    cmake_args.append("-DDIPU_ABI_V=" + get_DIPU_ABI_V())
-    cmake_args.append(
-        "-DDIPU_COMPILED_WITH_CXX11_ABI=" + str(get_DIPU_COMPILED_WITH_CXX11_ABI())
-    )
-    cmake_args.append("-DDIOPI_CMAKE_PREFIX_PATH=" + get_DIOPI_CMAKE_PREFIX_PATH())
-    cmake_args.append("-DPYTORCH_DIR=" + get_PYTORCH_DIR())
-    cmake_args.append("-DWITH_DIOPI_LIBRARY=" + cmake_with_diopi_library)
-    cmake_args.append("-DENABLE_COVERAGE=" + cmake_use_coverage)
-    return cmake_args
+
+    return [
+        "-DCMAKE_BUILD_TYPE=Release",
+        f"-DDEVICE={cmake_device}",
+        f"-DDIPU_ABI_V={dipu_abi_v()}",
+        f"-DDIPU_COMPILED_WITH_CXX11_ABI={dipu_compiled_with_cxx11_abi()}",
+        f"-DDIOPI_CMAKE_PREFIX_PATH={diopi_cmake_prefix_path()}",
+        f"-DPYTORCH_DIR={pytorch_dir()}",
+        f"-DWITH_DIOPI_LIBRARY={cmake_with_diopi_library}",
+        f"-DENABLE_COVERAGE={cmake_use_coverage}",
+    ]
 
 
 def torch_dipu_headers():
-    headers_pattern = list()
-    headers_pattern.append("csrc_dipu/*.h")
-    headers_pattern.append("csrc_dipu/aten/*.h")
-    headers_pattern.append("csrc_dipu/base/*.h")
-    headers_pattern.append("csrc_dipu/binding/*.h")
-    headers_pattern.append("csrc_dipu/diopirt/*.h")
-    headers_pattern.append("csrc_dipu/profiler/*.h")
-    headers_pattern.append("csrc_dipu/utils/*.h")
-    headers_pattern.append("csrc_dipu/runtime/*.h")
-    headers_pattern.append("csrc_dipu/runtime/core/*.h")
-    headers_pattern.append("csrc_dipu/runtime/core/allocator/*.h")
-    headers_pattern.append("csrc_dipu/runtime/core/guardimpl/*.h")
-    headers_pattern.append("csrc_dipu/runtime/device/*.h")
-    headers_pattern.append("csrc_dipu/runtime/devproxy/*.h")
-    headers_pattern.append("csrc_dipu/runtime/distributed/*.h")
-    headers_pattern.append("csrc_dipu/vendor/*/*.h")
-    return headers_pattern
+    return [
+        "csrc_dipu/*.h",
+        "csrc_dipu/aten/*.h",
+        "csrc_dipu/base/*.h",
+        "csrc_dipu/binding/*.h",
+        "csrc_dipu/diopirt/*.h",
+        "csrc_dipu/profiler/*.h",
+        "csrc_dipu/utils/*.h",
+        "csrc_dipu/runtime/*.h",
+        "csrc_dipu/runtime/core/*.h",
+        "csrc_dipu/runtime/core/allocator/*.h",
+        "csrc_dipu/runtime/core/guardimpl/*.h",
+        "csrc_dipu/runtime/device/*.h",
+        "csrc_dipu/runtime/devproxy/*.h",
+        "csrc_dipu/runtime/distributed/*.h",
+        "csrc_dipu/vendor/*/*.h",
+    ]
 
 
 setup(
     name="torch_dipu",
-    version=VERSION,
+    version="0.4.1",
     description="DIPU extension for PyTorch",
     url="https://github.com/DeepLink-org/dipu",
     packages=setuptools.find_packages(),
     cmake_args=customized_cmake_args(),
     cmake_install_target="all",
     package_data={
+        # TODO(lhy,wy): copy DIOPI so file and proto dir to install target dir.
+        # Note: only files located in python packages could be copied.
         "torch_dipu": [
-            "*.lib",
             "*.so",
-            "*.pylib",
-            "../third_party/DIOPI/impl/lib/*.lib",
-            "../third_party/DIOPI/impl/lib/*.so",
-            "../third_party/DIOPI/impl/lib/*.pylib",
         ]
         + torch_dipu_headers(),
     },
