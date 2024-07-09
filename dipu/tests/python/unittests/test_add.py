71169e1d8bd6d1a7fc8d4c0b3f8e55916a5491a3
zhaoguochun1995 <109069909+zhaoguochun1995@users.noreply.github.com>
2024-07-09 20:18:47 +0800

    Zgc/dipu fix unnecessary large memory tensor allocate and operation (#871)
    * fix unnecessary large memory tensor allocate and operation
    
    * fix div.out_mode device_check bug when self is cpu scalar
    
    * optimize div test case
    
    * Specify the div test case error threshold

--- dipu/tests/python/unittests/test_add.py
+++ dipu/tests/python/unittests/test_add.py
@@ -24,6 +24,13 @@ class TestAdd(TestCase):
         y.add_(torch.ones_like(y))
         self.assertEqual(x.cpu(), y)
 
+    def test_add_self_is_scalar(self):
+        x = torch.randn(3, 4).cuda()
+        y = x.cpu()
+        z_device = torch.add(2, x, alpha=0.3)
+        z_cpu = torch.add(2, y, alpha=0.3)
+        self.assertEqual(z_device.cpu(), z_cpu)
+
 
 if __name__ == "__main__":
     run_tests()
