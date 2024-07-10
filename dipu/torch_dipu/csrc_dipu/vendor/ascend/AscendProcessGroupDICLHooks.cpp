10b7bb915c8e3a473563b391e29c01026b53c830
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-07-10 09:20:24 +0000

    In Huawei hccl adaptation, avoid converting bool to int32 for communication and then converting int32 back to bool. The ne_ operator is used to avoid overflow when summing bool types, and the result is aligned with the cuda nccl communication result.

--- dipu/torch_dipu/csrc_dipu/vendor/ascend/AscendProcessGroupDICLHooks.cpp
+++ dipu/torch_dipu/csrc_dipu/vendor/ascend/AscendProcessGroupDICLHooks.cpp
@@ -14,6 +14,12 @@ void allReducePreFn(std::vector<std::shared_ptr<DICLComm>>& comms,
   if (inputs[0].scalar_type() == at::kByte) {
     DIPUStreamGuard guard(comms[0]->diclStream_.unwrap());
     outputs[0] = inputs[0].to(at::kShort);
+  } else if ((inputs[0].scalar_type() == at::kBool)) {
+    // To avoid overflow when summing bool data types, change the value of True
+    // to 1 and change the value of other places to 0. Generally, world_size
+    // will not exceed 256, so there will be no overflow.
+    DIPUStreamGuard guard(comms[0]->diclStream_.unwrap());
+    outputs[0].ne_(0);
   }
 }
 
@@ -23,6 +29,10 @@ void allReducePostFn(std::vector<std::shared_ptr<DICLComm>>& comms,
   if (inputs[0].scalar_type() != outputs[0].scalar_type()) {
     DIPUStreamGuard guard(comms[0]->diclStream_.unwrap());
     outputs[0].copy_(inputs[0]);
+  } else if ((inputs[0].scalar_type() == at::kBool)) {
+    // Make the bool type behavior aligned with cuda (1 is True, 0 is False)
+    DIPUStreamGuard guard(comms[0]->diclStream_.unwrap());
+    outputs[0].ne_(0);
   }
 }
 
