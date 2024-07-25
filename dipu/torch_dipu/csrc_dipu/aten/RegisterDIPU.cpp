bed5d90e031a1cf845fc56cd2dda5de129a835ba
fandaoyi <fandaoyi@sensetime.com>
2024-07-25 17:24:51 +0800

    comment4

--- dipu/torch_dipu/csrc_dipu/aten/RegisterDIPU.cpp
+++ dipu/torch_dipu/csrc_dipu/aten/RegisterDIPU.cpp
@@ -412,8 +412,8 @@ and to use muxi native cpu empty op to bypass this issue. see the doc below
 written by guochun1 for detail.
 https://aicarrier.feishu.cn/wiki/PXdYwcsjii9TJZk5AR1cG2bxnFd
 
-Warnning: Need check if this bypass works/needed when using dipu + torch_cpu which not
-contains 'mx' pin mem/cpu-empty op.
+Warnning: Need check if this bypass works/needed when using dipu + torch_cpu
+which not contains 'mx' pin mem/cpu-empty op.
 TODO: fandaoyi
 **/
 #if !DIPU_VENDOR_NAME_MUXI
